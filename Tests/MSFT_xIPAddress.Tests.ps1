c635b8f721bf8e6e9935fb375848508717a56fa0
Daniel Krebs <daniel.krebs@outlook.com>
2015-04-22 12:23:25 -0400

    Initial test script for MSFT_xIPAddress resource

---
+++ Tests/MSFT_xIPAddress.Tests.ps1
@@ -0,0 +1,91 @@
+﻿Remove-Module -Name MSFT_xIPAddress -Force -ErrorAction SilentlyContinue
+Import-Module -Name $PSScriptRoot\..\DSCResources\MSFT_xIPAddress -Force -DisableNameChecking
+
+InModuleScope MSFT_xIPAddress {
+
+    Describe 'Get-TargetResource' {
+    
+        #region Mocks
+        Mock Get-NetIPAddress {
+
+            [PSCustomObject]@{
+                IPAddress = '192.168.0.1'
+            }
+        }
+        #endregion
+
+        Context 'comparing IPAddress' {
+            It 'should return true' {
+
+                $Splat = @{
+                    IPAddress = '192.168.0.1'
+                    InterfaceAlias = 'Ethernet'
+                }
+                $Result = Get-TargetResource @Splat
+                $Result.IPAddress | Should Be $Splat.IPAddress
+            }
+        }
+    }
+
+
+	Describe 'ValidateProperties' {
+
+        #region Mocks
+	    Mock Get-NetIPAddress -MockWith {
+			
+		    [PSCustomObject]@{
+			    IPAddress = '192.168.0.1'
+			    InterfaceAlias = 'Ethernet'
+		    }
+	    }
+		
+	    Mock New-NetIPAddress -MockWith {}
+		
+	    Mock Get-NetConnectionProfile {
+		    [PSCustomObject]@{
+			    Name = 'MSFT'
+			    InterfaceAlias = 'Ethernet'
+			    InterfaceIndex = 1
+			    NetworkCategory = 'Public'
+			    IPV4Connectivity = 'Internet'
+			    IPV6Connectivity = 'NoTraffic'
+		    }
+	    }
+		
+	    Mock Set-NetConnectionProfile {}
+        #endregion
+		
+		Context 'invoking without -Apply switch' {
+			
+			It 'should be $false' {
+				$Splat = @{
+					IPAddres = '10.0.0.2'
+					InterfaceAlias = 'Ethernet'
+				}
+				$Result = ValidateProperties @Splat
+				$Result | Should Be $false
+			}
+			
+			It 'should be $true' {
+				$Splat = @{
+					IPAddres = '192.168.0.1'
+					InterfaceAlias = 'Ethernet'
+				}
+				$Result = ValidateProperties @Splat
+				$Result | Should Be $true
+			}
+		}
+		
+		Context 'invoking with -Apply switch' {
+			
+			It 'should be $null' {
+				$Splat = @{
+					IPAddres = '10.0.0.2'
+					InterfaceAlias = 'Ethernet'
+				}
+				$Result = ValidateProperties @Splat -Apply
+				$Result | Should BeNullOrEmpty
+			}
+		}
+	}
+}
