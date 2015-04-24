f71b00ccb4d37de44f86106f58182f6cd4e2772f
Daniel Krebs <daniel.krebs@outlook.com>
2015-04-23 22:14:25 -0400

    Fix space indentation

--- Tests/MSFT_xIPAddress.Tests.ps1
+++ Tests/MSFT_xIPAddress.Tests.ps1
@@ -28,64 +28,64 @@ InModuleScope MSFT_xIPAddress {
     }
 
 
-	Describe 'ValidateProperties' {
+    Describe 'ValidateProperties' {
 
         #region Mocks
-	    Mock Get-NetIPAddress -MockWith {
-			
-		    [PSCustomObject]@{
-			    IPAddress = '192.168.0.1'
-			    InterfaceAlias = 'Ethernet'
-		    }
-	    }
-		
-	    Mock New-NetIPAddress -MockWith {}
-		
-	    Mock Get-NetConnectionProfile {
-		    [PSCustomObject]@{
-			    Name = 'MSFT'
-			    InterfaceAlias = 'Ethernet'
-			    InterfaceIndex = 1
-			    NetworkCategory = 'Public'
-			    IPV4Connectivity = 'Internet'
-			    IPV6Connectivity = 'NoTraffic'
-		    }
-	    }
-		
-	    Mock Set-NetConnectionProfile {}
+        Mock Get-NetIPAddress -MockWith {
+            
+            [PSCustomObject]@{
+                IPAddress = '192.168.0.1'
+                InterfaceAlias = 'Ethernet'
+            }
+        }
+        
+        Mock New-NetIPAddress -MockWith {}
+        
+        Mock Get-NetConnectionProfile {
+            [PSCustomObject]@{
+                Name = 'MSFT'
+                InterfaceAlias = 'Ethernet'
+                InterfaceIndex = 1
+                NetworkCategory = 'Public'
+                IPV4Connectivity = 'Internet'
+                IPV6Connectivity = 'NoTraffic'
+            }
+        }
+        
+        Mock Set-NetConnectionProfile {}
         #endregion
-		
-		Context 'invoking without -Apply switch' {
-			
-			It 'should be $false' {
-				$Splat = @{
-					IPAddres = '10.0.0.2'
-					InterfaceAlias = 'Ethernet'
-				}
-				$Result = ValidateProperties @Splat
-				$Result | Should Be $false
-			}
-			
-			It 'should be $true' {
-				$Splat = @{
-					IPAddres = '192.168.0.1'
-					InterfaceAlias = 'Ethernet'
-				}
-				$Result = ValidateProperties @Splat
-				$Result | Should Be $true
-			}
-		}
-		
-		Context 'invoking with -Apply switch' {
-			
-			It 'should be $null' {
-				$Splat = @{
-					IPAddres = '10.0.0.2'
-					InterfaceAlias = 'Ethernet'
-				}
-				$Result = ValidateProperties @Splat -Apply
-				$Result | Should BeNullOrEmpty
-			}
-		}
-	}
+        
+        Context 'invoking without -Apply switch' {
+            
+            It 'should be $false' {
+                $Splat = @{
+                    IPAddres = '10.0.0.2'
+                    InterfaceAlias = 'Ethernet'
+                }
+                $Result = ValidateProperties @Splat
+                $Result | Should Be $false
+            }
+            
+            It 'should be $true' {
+                $Splat = @{
+                    IPAddres = '192.168.0.1'
+                    InterfaceAlias = 'Ethernet'
+                }
+                $Result = ValidateProperties @Splat
+                $Result | Should Be $true
+            }
+        }
+        
+        Context 'invoking with -Apply switch' {
+            
+            It 'should be $null' {
+                $Splat = @{
+                    IPAddres = '10.0.0.2'
+                    InterfaceAlias = 'Ethernet'
+                }
+                $Result = ValidateProperties @Splat -Apply
+                $Result | Should BeNullOrEmpty
+            }
+        }
+    }
 }
