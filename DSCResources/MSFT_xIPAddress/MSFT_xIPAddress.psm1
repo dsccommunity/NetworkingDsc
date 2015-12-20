5c338bea541038257050ecd80f92afb390415c0e
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-12-20 20:27:43 +1300

    Changed xIPAddress so that DHCP status is not checked for IPv6

--- DSCResources/MSFT_xIPAddress/MSFT_xIPAddress.psm1
+++ DSCResources/MSFT_xIPAddress/MSFT_xIPAddress.psm1
@@ -230,21 +230,27 @@ function Test-TargetResource
         }
     }
 
-    # Test if DHCP is already disabled
-    if (-not (Get-NetIPInterface `
-        -InterfaceAlias $InterfaceAlias `
-        -AddressFamily $AddressFamily).Dhcp.ToString().Equals('Disabled'))
+    # If DHCP is still enabled on IPv4 then IP address changes are definitely needed
+    # This test doesn't apply to IPv6 because the DHCP parameter returned on an IPv6
+    # does not mean DHCP is actually active.
+    if ($AddressFamily -eq 'IPv4')
     {
-        Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
-            $($LocalizedData.DHCPIsNotDisabledMessage)
-            ) -join '' )
-        $desiredConfigurationMatch = $false
-    }
-    else
-    {
-        Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
-            $($LocalizedData.DHCPIsAlreadyDisabledMessage)
-            ) -join '' )
+        # Test if DHCP is already disabled
+        if (-not (Get-NetIPInterface `
+            -InterfaceAlias $InterfaceAlias `
+            -AddressFamily $AddressFamily).Dhcp.ToString().Equals('Disabled'))
+        {
+            Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
+                $($LocalizedData.DHCPIsNotDisabledMessage)
+                ) -join '' )
+            $desiredConfigurationMatch = $false
+        }
+        else
+        {
+            Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
+                $($LocalizedData.DHCPIsAlreadyDisabledMessage)
+                ) -join '' )
+        }
     }
     return $desiredConfigurationMatch
 } # Test-TargetResource
