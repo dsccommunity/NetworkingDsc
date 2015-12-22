d9ce40698d673852703451d7623f28a728d21f5e
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-12-22 16:17:29 +1300

    xDHCPClient Resource Added.

--- DSCResources/MSFT_xIPAddress/MSFT_xIPAddress.psm1
+++ DSCResources/MSFT_xIPAddress/MSFT_xIPAddress.psm1
@@ -230,35 +230,6 @@ function Test-TargetResource
                 ) -join '' )
         }
     }
-
-    # If DHCP is still enabled on IPv4 then IP address changes are definitely needed
-    # This test doesn't apply to IPv6 because the DHCP parameter returned on an IPv6
-    # does not mean DHCP is actually active.
-    if ($AddressFamily -eq 'IPv4')
-    {
-        # Test if DHCP is already disabled
-        if (-not (Get-NetIPInterface `
-            -InterfaceAlias $InterfaceAlias `
-            -AddressFamily $AddressFamily).Dhcp.ToString().Equals('Disabled'))
-        {
-            Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
-                $($LocalizedData.DHCPIsNotDisabledMessage)
-                ) -join '' )
-            $desiredConfigurationMatch = $false
-        }
-        else
-        {
-            Write-Verbose -Message ( @( "$($MyInvocation.MyCommand): "
-                $($LocalizedData.DHCPIsAlreadyDisabledMessage)
-                ) -join '' )
-        }
-    }
-    else
-    {
-        Write-Warning -Message ( @( "$($MyInvocation.MyCommand): "
-            $($LocalizedData.DHCPIsNotTestedMessage)
-            ) -join '' )        
-    }
     return $desiredConfigurationMatch
 } # Test-TargetResource
 
