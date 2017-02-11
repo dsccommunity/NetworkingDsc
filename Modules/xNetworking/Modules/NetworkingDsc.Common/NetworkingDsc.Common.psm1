4f3338cdd8d9311adc06cc782082b5de1509f346
Raimund Andée <raandree@live.com>
2017-02-12 00:37:46 +0100

    New function to Common.psm1
    Test-DSCObjectHasProperty
    Test-DscParameterState
    New-TerminatingError

--- Modules/xNetworking/Modules/NetworkingDsc.Common/NetworkingDsc.Common.psm1
+++ Modules/xNetworking/Modules/NetworkingDsc.Common/NetworkingDsc.Common.psm1
@@ -9,12 +9,12 @@ $script:localizedData = Get-LocalizedData `
     -ResourcePath $PSScriptRoot
 
 <#
-    .SYNOPSIS
-    Converts any IP Addresses containing CIDR notation filters in an array to use Subnet Mask
-    notation.
+        .SYNOPSIS
+        Converts any IP Addresses containing CIDR notation filters in an array to use Subnet Mask
+        notation.
 
-    .PARAMETER Address
-    The array of addresses to that need to be converted.
+        .PARAMETER Address
+        The array of addresses to that need to be converted.
 #>
 function Convert-CIDRToSubhetMask
 {
@@ -69,5 +69,145 @@ function Convert-CIDRToSubhetMask
     return $Results
 }
 
-Export-ModuleMember -Function `
-    Convert-CIDRToSubhetMask
+function Test-DscParameterState
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)] 
+        [HashTable]$CurrentValues,
+	
+        [Parameter(Mandatory)] 
+        [Object]
+        $DesiredValues,
+        
+        [string[]]$ValuesToCheck
+    )
+	
+    $returnValue = $true
+	
+    $types = 'System.Management.Automation.PSBoundParametersDictionary', 'System.Collections.Hashtable', 'Microsoft.Management.Infrastructure.CimInstance'
+    
+    if ($DesiredValues.GetType().FullName -notin $types)
+    {
+        throw ("Property 'DesiredValues' in Test-SPDscParameterState must be either a Hashtable or CimInstance. Type detected was $($DesiredValues.GetType().Name)")
+    }
+	
+    if ($DesiredValues.GetType().FullName -eq 'Microsoft.Management.Infrastructure.CimInstance' -and -not $ValuesToCheck)
+    {
+        throw ("If 'DesiredValues' is a CimInstance then property 'ValuesToCheck' must contain a value")
+    }
+    
+    if ($DesiredValues.ContainsKey('Verbose')) { $null = $DesiredValues.Remove('Verbose') }
+	
+    if (-not $ValuesToCheck)
+    { $keyList = $DesiredValues.Keys } 
+    else
+    { $keyList = $ValuesToCheck }
+	
+    foreach ($key in $keyList)
+    {
+        #check for verbose key?
+        
+        if ($CurrentValues.$key -eq $DesiredValues.$key)
+        {
+            Write-Verbose -Message "MATCH: $($desiredType.Name) value for property '$key' does match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValues.$key)'"
+            continue
+        }
+                    
+        if ($DesiredValues.GetType().Name -in 'HashTable', 'PSBoundParametersDictionary')
+        {
+            $checkDesiredValue = $DesiredValues.ContainsKey($key)
+        } 
+        else
+        {
+            $checkDesiredValue = Test-DSCObjectHasProperty -Object $DesiredValues -PropertyName $key
+        }
+        
+        if (-not $checkDesiredValue)
+        {
+            Write-Verbose -Message "MATCH: $($desiredType.Name) value for property '$key' does match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValues.$key)'"
+            continue
+        }
+        
+ 
+        $desiredType = $DesiredValues.$key.GetType()
+            
+        if ($desiredType.IsArray)
+        {
+            if (-not $CurrentValues.ContainsKey($key) -or -not $CurrentValues.$key)
+            {
+                Write-Verbose -Message "Expected to find an array value for property '$key' in the current values, but it was either not present or was null. This has caused the test method to return false."
+                $returnValue = $false
+            } 
+            else
+            {
+                $arrayCompare = Compare-Object -ReferenceObject $CurrentValues.$key -DifferenceObject $DesiredValues.$key
+                if (-not $arrayCompare) 
+                {
+                    Write-Verbose -Message "Found an array for property '$key' in the current values, but this array does not match the desired state. Details of the changes are below."
+                    foreach ($entry in $arrayCompare)
+                    {
+                        #does this work?
+                        Write-Verbose -Message "$($entry.InputObject) - $($key.SideIndicator)"
+                    }
+                    $returnValue = $false
+                }
+            }
+        } 
+        else {
+            if ($DesiredValues.$key -ne $CurrentValues.$key)
+            {
+                Write-Verbose -Message "NOTMATCH: $($desiredType.Name) value for property '$key' does not match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValues.$key)'"
+                $returnValue = $false
+            }
+        
+        } 
+    }
+    
+    Write-Verbose "Result is '$returnValue'"
+    return $returnValue
+}
+
+function Test-DSCObjectHasProperty
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory)] 
+        [object]$Object,
+	
+        [Parameter(Mandatory)]
+        [string]$PropertyName
+    )
+	
+    if ($Object.PSObject.Properties.Name -contains $PropertyName) 
+    {
+        return [bool]$Object.$PropertyName
+    }
+    
+    return $false
+}
+
+function New-TerminatingError
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$ErrorId,
+
+        [Parameter(Mandatory)]
+        [string]$ErrorMessage,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.ErrorCategory]$ErrorCategory
+    )
+
+    $exception = New-Object System.InvalidOperationException $errorMessage
+    $errorRecord = New-Object System.Management.Automation.ErrorRecord $exception, $errorId, $errorCategory, $null
+    throw $errorRecord
+}
+
+#Export-ModuleMember -Function Convert-CIDRToSubhetMask
