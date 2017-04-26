44218a3aae24d47064c3e4015c2aed7abe2a0445
Håvard Ola Eggen <havard.ola.eggen@gmail.com>
2017-04-26 14:40:33 +0200

    The hashtable returned from Get-DscConfig have to match what is specified in the mof. Probably have to redo some of the unit tests.

--- Modules/xNetworking/DSCResources/MSFT_xNetAdapterLso/MSFT_xNetAdapterLso.psm1
+++ Modules/xNetworking/DSCResources/MSFT_xNetAdapterLso/MSFT_xNetAdapterLso.psm1
@@ -59,11 +59,12 @@ function Get-TargetResource
 
             $result = @{ 
                 Name = $Name
+                Protocol = $Protocol
             }
             switch ($Protocol) {
-                "V1IPv4" { $result.add('V1IPv4Enabled', $netAdapter.V1IPv4Enabled) }
-                "IPv4"   { $result.add('IPv4Enabled', $netAdapter.IPv4Enabled) }
-                "IPv6"   { $result.add('IPv6Enabled', $netAdapter.IPv6Enabled) }
+                "V1IPv4" { $result.add('State', $netAdapter.V1IPv4Enabled) }
+                "IPv4"   { $result.add('State', $netAdapter.IPv4Enabled) }
+                "IPv6"   { $result.add('State', $netAdapter.IPv6Enabled) }
                 Default {"Should not be called."}
             }
             return $result
