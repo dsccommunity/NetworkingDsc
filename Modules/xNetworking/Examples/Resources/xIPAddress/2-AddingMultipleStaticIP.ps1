53c597bd28a9912864c40118e49a9008695775e3
Chris Gardner <chris@blackmarble.co.uk>
2017-04-27 17:23:26 +0100

    Adding example

---
+++ Modules/xNetworking/Examples/Resources/xIPAddress/2-AddingMultipleStaticIP.ps1
@@ -0,0 +1,33 @@
+<#
+    .EXAMPLE
+    Disabling DHCP and adding multiple static IP Addresses for IPv6
+#>
+Configuration Example
+{
+    param
+    (
+        [Parameter()]
+        [System.String[]]
+        $NodeName = 'localhost'
+    )
+
+    Import-DscResource -Module xNetworking
+
+    Node $NodeName
+    {
+        xDhcpClient DisabledDhcpClient
+        {
+            State          = 'Disabled'
+            InterfaceAlias = 'Ethernet'
+            AddressFamily  = 'IPv6'
+        }
+
+        xIPAddress NewIPAddress
+        {
+            IPAddress      = '2001:4898:200:7:6c71:a102:ebd8:f482','2001:4598:210:7:6d71:a102:ebe8:f483'
+            InterfaceAlias = 'Ethernet'
+            PrefixLength   = 24
+            AddressFamily  = 'IPV6'
+        }
+    }
+}
