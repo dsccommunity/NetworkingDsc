0ffe8d9c37632613f9dc38457de3e43a6d57840b
Raimund Andée [MSFT] <raandree@live.com>
2017-06-07 20:13:08 +0200

    Removing tabs and adding line at the end

--- Modules/xNetworking/Modules/NetworkingDsc.Common/NetworkingDsc.Common.psm1
+++ Modules/xNetworking/Modules/NetworkingDsc.Common/NetworkingDsc.Common.psm1
@@ -2,7 +2,7 @@
         .SYNOPSIS
         Converts any IP Addresses containing CIDR notation filters in an array to use Subnet Mask
         notation.
-	
+
         .PARAMETER Address
         The array of addresses to that need to be converted.
 #>
@@ -16,7 +16,7 @@ function Convert-CIDRToSubhetMask
         [ValidateNotNullOrEmpty()]
         [String[]] $Address
     )
-	
+
     $Results = @()
     foreach ($Entry in $Address)
     {
@@ -58,40 +58,40 @@ function Convert-CIDRToSubhetMask
     }
     return $Results
 } # Convert-CIDRToSubhetMask
-	
+
 <#
         .SYNOPSIS
         This function will find a network adapter based on the provided
         search parameters.
-	
+
         .PARAMETER Name
         This is the name of network adapter to find.
-	
+
         .PARAMETER PhysicalMediaType
         This is the media type of the network adapter to find.
-	
+
         .PARAMETER Status
         This is the status of the network adapter to find.
-	
+
         .PARAMETER MacAddress
         This is the MAC address of the network adapter to find.
-	
+
         .PARAMETER InterfaceDescription
         This is the interface description of the network adapter to find.
-	
+
         .PARAMETER InterfaceIndex
         This is the interface index of the network adapter to find.
-	
+
         .PARAMETER InterfaceGuid
         This is the interface GUID of the network adapter to find.
-	
+
         .PARAMETER DriverDescription
         This is the driver description of the network adapter.
-	
+
         .PARAMETER InterfaceNumber
         This is the interface number of the network adapter if more than one
         are returned by the parameters.
-	
+
         .PARAMETER IgnoreMultipleMatchingAdapters
         This switch will suppress an error occurring if more than one matching
         adapter matches the parameters passed.
@@ -105,96 +105,96 @@ function Find-NetworkAdapter
         [Parameter()]
         [System.String]
         $Name,
-	
+
         [Parameter()]
         [System.String]
         $PhysicalMediaType,
-	
+
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [ValidateSet('Up','Disconnected','Disabled')]
         [System.String]
         $Status = 'Up',
-	
+
         [Parameter()]
         [System.String]
         $MacAddress,
-	
+
         [Parameter()]
         [System.String]
         $InterfaceDescription,
-	
+
         [Parameter()]
         [System.UInt32]
         $InterfaceIndex,
-	
+
         [Parameter()]
         [System.String]
         $InterfaceGuid,
-	
+
         [Parameter()]
         [System.String]
         $DriverDescription,
-	
+
         [Parameter()]
         [System.UInt32]
         $InterfaceNumber = 1,
-	
+
         [Parameter()]
         [System.Boolean]
         $IgnoreMultipleMatchingAdapters = $false
     )
-	
+
     Write-Verbose -Message ( @("$($MyInvocation.MyCommand): "
             $($LocalizedData.FindingNetAdapterMessage)
     ) -join '')
-	
+
     $adapterFilters = @()
     if($PSBoundParameters.ContainsKey('Name'))
     {
         $adapterFilters += @('($_.Name -eq $Name)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('PhysicalMediaType'))
     {
         $adapterFilters += @('($_.PhysicalMediaType -eq $PhysicalMediaType)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('Status')) {
         $adapterFilters += @('($_.Status -eq $Status)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('MacAddress'))
     {
         $adapterFilters += @('($_.MacAddress -eq $MacAddress)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('InterfaceDescription'))
     {
         $adapterFilters += @('($_.InterfaceDescription -eq $InterfaceDescription)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('InterfaceIndex'))
     {
         $adapterFilters += @('($_.InterfaceIndex -eq $InterfaceIndex)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('InterfaceGuid'))
     {
         $adapterFilters += @('($_.InterfaceGuid -eq $InterfaceGuid)')
     } # if
-	
+
     if($PSBoundParameters.ContainsKey('DriverDescription'))
     {
         $adapterFilters += @('($_.DriverDescription -eq $DriverDescription)')
     } # if
-	
+
     if ($adapterFilters.Count -eq 0)
     {
         Write-Verbose -Message ( @("$($MyInvocation.MyCommand): "
                 $($LocalizedData.AllNetAdaptersFoundMessage)
         ) -join '')
-	
+
         $matchingAdapters = @(Get-NetAdapter)
     }
     else
@@ -204,13 +204,13 @@ function Find-NetworkAdapter
         $matchingAdapters = @(Get-NetAdapter |
         Where-Object -FilterScript ([ScriptBlock]::Create($adapterFilterScript)))
     }
-	
+
     # Were any adapters found matching the criteria?
     if ($matchingAdapters.Count -eq 0)
     {
         New-InvalidOperationException `
         -Message ($LocalizedData.NetAdapterNotFoundError)
-	
+
         # Return a null so that ErrorAction SilentlyContinue works correctly
         return $null
     }
@@ -219,7 +219,7 @@ function Find-NetworkAdapter
         Write-Verbose -Message ( @("$($MyInvocation.MyCommand): "
                 $($LocalizedData.NetAdapterFoundMessage -f $matchingAdapters.Count)
         ) -join '')
-	
+
         if ($matchingAdapters.Count -gt 1)
         {
             if ($IgnoreMultipleMatchingAdapters)
@@ -230,7 +230,7 @@ function Find-NetworkAdapter
                     New-InvalidOperationException `
                     -Message ($LocalizedData.InvalidNetAdapterNumberError `
                     -f $matchingAdapters.Count,$InterfaceNumber)
-	
+
                     # Return a null so that ErrorAction SilentlyContinue works correctly
                     return $null
                 } # if
@@ -240,16 +240,16 @@ function Find-NetworkAdapter
                 New-InvalidOperationException `
                 -Message ($LocalizedData.MultipleMatchingNetAdapterFound `
                 -f $matchingAdapters.Count)
-	
+
                 # Return a null so that ErrorAction SilentlyContinue works correctly
                 return $null
             } # if
         } # if
     } # if
-	
+
     # Identify the exact adapter from the adapters that match
     $exactAdapter = $matchingAdapters[$InterfaceNumber - 1]
-	
+
     $returnValue = [PSCustomObject] @{
         Name = $exactAdapter.Name
         PhysicalMediaType = $exactAdapter.PhysicalMediaType
@@ -260,7 +260,7 @@ function Find-NetworkAdapter
         InterfaceGuid = $exactAdapter.InterfaceGuid
         MatchingAdapterCount = $matchingAdapters.Count
     }
-	
+
     return $returnValue
 } # Find-NetworkAdapter
 
@@ -463,6 +463,7 @@ function Test-DscParameterState
     Write-Verbose "Result is '$returnValue'"
     return $returnValue
 }
+
 
 function Test-DSCObjectHasProperty
 {
