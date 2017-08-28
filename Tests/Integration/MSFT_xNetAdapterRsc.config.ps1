1128e8079f5d94f827e2743c5c747be720006bd9
atdhe18 <atdhe.95@hotmail.com>
2017-08-28 14:11:55 +0200

    Added NetAdapterRsc Intergration Test Files

---
+++ Tests/Integration/MSFT_xNetAdapterRsc.config.ps1
@@ -0,0 +1,17 @@
+﻿$NetAdapter = Get-NetAdapter | Where-Object {$_.NdisVersion -ge 6} | Select-Object -First 1
+$TestEnableRscIPv6 = [PSObject]@{
+    Name     = $NetAdapter.Name
+    Protocol = 'IPv6'
+    State    = $true
+}
+
+configuration MSFT_xNetAdapterRsc_Config {
+    Import-DscResource -ModuleName xNetworking
+    node localhost {
+        xNetAdapterRsc Integration_Test {
+            Name        = $TestEnableRscIPv6.Name
+            Protocol    = $TestEnableRscIPv6.Protocol
+            State       = $TestEnableRscIPv6.State
+        }
+    }
+}
