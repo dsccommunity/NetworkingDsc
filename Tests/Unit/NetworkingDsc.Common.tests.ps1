588f350d03c6f2928202e06354f547964aeab466
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-09-23 22:04:18 +1200

    Created Set-ComputerProxySettings

--- Tests/Unit/NetworkingDsc.Common.tests.ps1
+++ Tests/Unit/NetworkingDsc.Common.tests.ps1
@@ -19,7 +19,6 @@ Import-Module (Join-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath (J
 try
 {
     #region Pester Tests
-
     $LocalizedData = InModuleScope $script:ModuleName {
         $LocalizedData
     }
@@ -800,6 +799,26 @@ try
 
                 $IPaddress.IPaddress | Should be 'FF12::12::123'
                 $IPaddress.PrefixLength | Should be 64
+            }
+        }
+
+        InModuleScope $script:ModuleName {
+            Describe 'NetworkingDsc.Common\Get-ProxySettings' {
+                Context 'Specified registry key value is null' {
+                    $convertToStringParameters = @{
+                        RegistryKeyValue = $null
+                    }
+
+                    It 'Should not throw' {
+                        { $null = ConvertTo-String @convertToStringParameters } | Should Not Throw
+                    }
+
+                    $convertToStringResult = ConvertTo-String @convertToStringParameters
+
+                    It 'Should return an empty string' {
+                        $convertToStringResult | Should Be ([String]::Empty)
+                    }
+                }
             }
         }
     }
