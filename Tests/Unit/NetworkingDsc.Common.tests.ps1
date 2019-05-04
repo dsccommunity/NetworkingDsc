c3d9376c2abb38c50aff29ccd8aea7a8826d7f6f
Daniel Scott-Raynsford <plagueho@gmail.com>
2019-05-04 20:27:47 +1200

    Added unit tests for Get-LocalizationData

--- Tests/Unit/NetworkingDsc.Common.tests.ps1
+++ Tests/Unit/NetworkingDsc.Common.tests.ps1
@@ -1227,6 +1227,100 @@ try
                 }
             }
         }
+
+        Describe 'DscResource.Common\Get-LocalizedData' {
+            $mockTestPath = {
+                return $mockTestPathReturnValue
+            }
+
+            $mockImportLocalizedData = {
+                $BaseDirectory | Should -Be $mockExpectedLanguagePath
+            }
+
+            BeforeEach {
+                Mock -CommandName Test-Path -MockWith $mockTestPath -Verifiable
+                Mock -CommandName Import-LocalizedData -MockWith $mockImportLocalizedData -Verifiable
+            }
+
+            Context 'When loading localized data for Swedish' {
+                $mockExpectedLanguagePath = 'sv-SE'
+                $mockTestPathReturnValue = $true
+
+                It 'Should call Import-LocalizedData with sv-SE language' {
+                    Mock -CommandName Join-Path -MockWith {
+                        return 'sv-SE'
+                    } -Verifiable
+
+                    { Get-LocalizedData -ResourceName 'DummyResource' } | Should -Not -Throw
+
+                    Assert-MockCalled -CommandName Join-Path -Exactly -Times 3 -Scope It
+                    Assert-MockCalled -CommandName Test-Path -Exactly -Times 1 -Scope It
+                    Assert-MockCalled -CommandName Import-LocalizedData -Exactly -Times 1 -Scope It
+                }
+
+                $mockExpectedLanguagePath = 'en-US'
+                $mockTestPathReturnValue = $false
+
+                It 'Should call Import-LocalizedData and fallback to en-US if sv-SE language does not exist' {
+                    Mock -CommandName Join-Path -MockWith {
+                        return $ChildPath
+                    } -Verifiable
+
+                    { Get-LocalizedData -ResourceName 'DummyResource' } | Should -Not -Throw
+
+                    Assert-MockCalled -CommandName Join-Path -Exactly -Times 4 -Scope It
+                    Assert-MockCalled -CommandName Test-Path -Exactly -Times 1 -Scope It
+                    Assert-MockCalled -CommandName Import-LocalizedData -Exactly -Times 1 -Scope It
+                }
+
+                Context 'When $ScriptRoot is set to a path' {
+                    $mockExpectedLanguagePath = 'sv-SE'
+                    $mockTestPathReturnValue = $true
+
+                    It 'Should call Import-LocalizedData with sv-SE language' {
+                        Mock -CommandName Join-Path -MockWith {
+                            return 'sv-SE'
+                        } -Verifiable
+
+                        { Get-LocalizedData -ResourceName 'DummyResource' -ScriptRoot '.' } | Should -Not -Throw
+
+                        Assert-MockCalled -CommandName Join-Path -Exactly -Times 1 -Scope It
+                        Assert-MockCalled -CommandName Test-Path -Exactly -Times 1 -Scope It
+                        Assert-MockCalled -CommandName Import-LocalizedData -Exactly -Times 1 -Scope It
+                    }
+
+                    $mockExpectedLanguagePath = 'en-US'
+                    $mockTestPathReturnValue = $false
+
+                    It 'Should call Import-LocalizedData and fallback to en-US if sv-SE language does not exist' {
+                        Mock -CommandName Join-Path -MockWith {
+                            return $ChildPath
+                        } -Verifiable
+
+                        { Get-LocalizedData -ResourceName 'DummyResource' -ScriptRoot '.' } | Should -Not -Throw
+
+                        Assert-MockCalled -CommandName Join-Path -Exactly -Times 2 -Scope It
+                        Assert-MockCalled -CommandName Test-Path -Exactly -Times 1 -Scope It
+                        Assert-MockCalled -CommandName Import-LocalizedData -Exactly -Times 1 -Scope It
+                    }
+                }
+            }
+
+            Context 'When loading localized data for English' {
+                Mock -CommandName Join-Path -MockWith {
+                    return 'en-US'
+                } -Verifiable
+
+                $mockExpectedLanguagePath = 'en-US'
+                $mockTestPathReturnValue = $true
+
+                It 'Should call Import-LocalizedData with en-US language' {
+                    { Get-LocalizedData -ResourceName 'DummyResource' } | Should -Not -Throw
+                }
+            }
+
+            Assert-VerifiableMock
+        }
     }
 }
 finally
