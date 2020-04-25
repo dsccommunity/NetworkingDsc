be4d571bcd4c26c7c4142835db154e6b92d98b3f
Greg Valainis <valainisgt@tutanota.com>
2020-04-25 13:23:05 -0400

    Added test case for IPv6 mismatch error

--- tests/Unit/NetworkingDsc.Common.tests.ps1
+++ tests/Unit/NetworkingDsc.Common.tests.ps1
@@ -2399,5 +2399,17 @@ InModuleScope $script:subModuleName {
                 { Test-IPAddress -Address $Address -AddressFamily $addressFamily } | Should -Throw $errorRecord
             }
         }
+        Context 'When address is valid IPv6 and IPv4 family' {
+            It 'Should throw AddressMismatchError' {
+                $address = '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
+                $addressFamily = 'IPv4'
+
+                $errorRecord = Get-InvalidArgumentRecord `
+                        -Message ($script:localizedData.AddressIPv6MismatchError -f $Address, $AddressFamily) `
+                        -ArgumentName 'AddressFamily'
+
+                { Test-IPAddress -Address $Address -AddressFamily $addressFamily } | Should -Throw $errorRecord
+            }
+        }
     }
 }
