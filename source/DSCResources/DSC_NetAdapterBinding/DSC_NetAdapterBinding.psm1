fde27137222d49b1dfbc9241e27a3fac10e1a226
John D Pell <John+git@gaelicWizard.net>
2021-05-22 16:42:08 -0400

    NetAdapterbinding: use Find-NetworkAdapter
    In Get-Binding, use Find-NetworkAdapter instead of Get-NetAdapter.

--- source/DSCResources/DSC_NetAdapterBinding/DSC_NetAdapterBinding.psm1
+++ source/DSCResources/DSC_NetAdapterBinding/DSC_NetAdapterBinding.psm1
@@ -262,7 +262,7 @@ function Get-Binding
         $State = 'Enabled'
     )
 
-    if (-not (Get-NetAdapter -Name $InterfaceAlias -ErrorAction SilentlyContinue))
+    if (-not (Find-NetworkAdapter -Name $InterfaceAlias -ErrorAction SilentlyContinue))
     {
         New-InvalidArgumentException `
             -Message ($script:localizedData.InterfaceNotAvailableError -f $InterfaceAlias) `
