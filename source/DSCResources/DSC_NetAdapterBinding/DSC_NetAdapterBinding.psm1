d24c9233758bdb868df72be0c6ccaacc7363feee
John D Pell <John+git@gaelicWizard.net>
2021-05-22 16:36:17 -0400

    NetAdapterBinding: include hidden adapters
    Add -IncludeHidden:$true to calls to *-NetAdapterBinding

--- source/DSCResources/DSC_NetAdapterBinding/DSC_NetAdapterBinding.psm1
+++ source/DSCResources/DSC_NetAdapterBinding/DSC_NetAdapterBinding.psm1
@@ -126,7 +126,8 @@ function Set-TargetResource
 
     if ($State -eq 'Enabled')
     {
-        Enable-NetAdapterBinding @PSBoundParameters
+        Enable-NetAdapterBinding @PSBoundParameters `
+            -IncludeHidden:$true
 
         Write-Verbose -Message ( @("$($MyInvocation.MyCommand): "
                 $($script:localizedData.NetAdapterBindingEnabledMessage -f `
@@ -135,7 +136,8 @@ function Set-TargetResource
     }
     else
     {
-        Disable-NetAdapterBinding @PSBoundParameters
+        Disable-NetAdapterBinding @PSBoundParameters `
+            -IncludeHidden:$true
 
         Write-Verbose -Message ( @("$($MyInvocation.MyCommand): "
                 $($script:localizedData.NetAdapterBindingDisabledMessage -f `
@@ -269,6 +271,7 @@ function Get-Binding
 
     $binding = Get-NetAdapterBinding `
         -InterfaceAlias $InterfaceAlias `
+        -IncludeHidden:$true `
         -ComponentId $ComponentId `
         -ErrorAction Stop
 
