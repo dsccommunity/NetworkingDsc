82cd36d7f9fe379ca3ae3a585cbd39c8a161e0a1
Chris Hunt <github@automatedops.com>
2024-06-11 11:03:29 -0400

    Fixing tests

--- tests/Integration/DSC_HostsFile.Integration.Tests.ps1
+++ tests/Integration/DSC_HostsFile.Integration.Tests.ps1
@@ -180,11 +180,11 @@ try
             }
 
             It 'Should include the extra whitespace' {
-                "${env:SystemRoot}\System32\Drivers\Etc\Hosts" | Should -FileContentMatch " $($configData.AllNodes[0].IPAddress) $($configData.AllNodes[0].HostName)"
+                "${env:SystemRoot}\System32\Drivers\Etc\Hosts" | Should -FileContentMatch " $($configData.AllNodes[0].IPAddress)`t$($configData.AllNodes[0].HostName)"
             }
 
             It 'Should not fail subsiquent Test invocations' {
-                $result = Test-DscConfiguration -Path $TestDrive -Wait -Verbose
+                $result = Test-DscConfiguration -Path $TestDrive
                 $result | Should -BeTrue
             }
         }
