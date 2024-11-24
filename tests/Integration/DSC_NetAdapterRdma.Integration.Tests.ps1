a4dd90e5456fadc6719a56eacf054e9033b18e71
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2024-11-24 18:43:18 +0000

    Update integration tests

--- tests/Integration/DSC_NetAdapterRdma.Integration.Tests.ps1
+++ tests/Integration/DSC_NetAdapterRdma.Integration.Tests.ps1
@@ -11,45 +11,83 @@
     testing, so do not specify an adapter used for connectivity to the test client. This
     is why these tests can not be executed in AppVeyor.
 #>
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_NetAdapterRdma'
-$script:AdapterName = 'vEthernet (Default Switch)'
 
-# Check the adapter selected for use in testing is RDMA compatible and preserve state
-$script:adapterRDMAStatus = Get-NetAdapterRdma -Name $script:AdapterName -ErrorAction SilentlyContinue
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-if (-not $script:adapterRDMAStatus)
-{
-    Write-Verbose -Message ('The network adapter selected for RDMA integration testing is not RDMA compatible. Integration tests will be skipped.')
-    return
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'NetAdapterRdma'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:AdapterName = 'vEthernet (Default Switch)'
+
+    # Check the adapter selected for use in testing is RDMA compatible and preserve state
+    $script:adapterRDMAStatus = Get-NetAdapterRdma -Name $script:AdapterName -ErrorAction SilentlyContinue
+
+    if (-not $script:adapterRDMAStatus)
+    {
+        $script:skip = $true
+    }
 }
 
-# Make sure RDMA is disabled on the selected adapter before running tests
-Set-NetAdapterRdma -Name $script:AdapterName -Enabled $false
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'NetAdapterRdma'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    . $configFile
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+    $script:AdapterName = 'vEthernet (Default Switch)'
+
+    # Check the adapter selected for use in testing is RDMA compatible and preserve state
+    $script:adapterRDMAStatus = Get-NetAdapterRdma -Name $script:AdapterName -ErrorAction SilentlyContinue
 }
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-
-# Begin Testing
-try
-{
-    Describe 'NetAdapterName Integration Tests' {
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile -Verbose -ErrorAction Stop
+Describe 'NetAdapterName Integration Tests' -Skip:$script:skip {
+    BeforeAll {
+        # Make sure RDMA is disabled on the selected adapter before running tests
+        Set-NetAdapterRdma -Name $script:AdapterName -Enabled $false
 
         # This is to pass to the Config
         $configData = @{
@@ -61,47 +99,45 @@ try
                 }
             )
         }
+    }
 
-        Describe "$($script:dscResourceName)_Integration" {
-            AfterAll {
-                Set-NetAdapterRdma `
-                    -Name $configData.AllNodes[0].Name `
-                    -Enabled $false
+    AfterAll {
+        Set-NetAdapterRdma -Name $script:AdapterName -Enabled $script:adapterRDMAStatus.Enabled
+    }
+
+    Describe "$($script:dscResourceName)_Integration" {
+        AfterAll {
+            Set-NetAdapterRdma `
+                -Name $configData.AllNodes[0].Name `
+                -Enabled $false
+        }
+
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configData
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all the parameters should match' {
+            $result = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
             }
-
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
-
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force `
-                        -ErrorAction Stop
-                } | Should -Not -Throw
-            }
-
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
-
-            It 'Should have set the resource and all the parameters should match' {
-                $result = Get-DscConfiguration | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-                $result.Name                   | Should -Be $configData.AllNodes[0].Name
-                $result.Enabled                | Should -Be $configData.AllNodes[0].Enabled
-            }
+            $result.Name                   | Should -Be $configData.AllNodes[0].Name
+            $result.Enabled                | Should -Be $configData.AllNodes[0].Enabled
         }
     }
-}
-finally
-{
-    Set-NetAdapterRdma -Name $script:AdapterName -Enabled $script:adapterRDMAStatus.Enabled
-
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
--- tests/Integration/DSC_NetAdapterState.Integration.Tests.ps1
+++ tests/Integration/DSC_NetAdapterState.Integration.Tests.ps1
@@ -1,64 +1,90 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_NetAdapterState'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
-}
-
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-
-# Begin Testing
-try
-{
-    Describe 'NetAdapterState Integration Tests' {
-        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
-
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile -Verbose -ErrorAction Stop
-
-        Describe "$($script:dscResourceName)_Integration" {
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
-
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force `
-                        -ErrorAction Stop
-                } | Should -Not -Throw
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
-
-            It 'Should have set the resource and all the parameters should match' {
-                $current = Get-DscConfiguration | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-                $current.Name     | Should -Be $TestNetAdapterState.Name
-                $current.State    | Should -Be $TestNetAdapterState.State
-            }
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
         }
     }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'NetAdapterState'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 }
-finally
-{
+
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'NetAdapterState'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    . $configFile
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+    New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+}
+
+AfterAll {
     Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
 
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+
     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
+
+Describe 'NetAdapterState Integration Tests' {
+    Describe "$($script:dscResourceName)_Integration" {
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+            }
+            $current.Name     | Should -Be $TestNetAdapterState.Name
+            $current.State    | Should -Be $TestNetAdapterState.State
+        }
+    }
 }
