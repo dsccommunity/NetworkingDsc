f504d8f62a67205ddea8c46b530659ea2da58801
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2024-11-24 16:02:15 +0000

    Update DefaultGateway Integration Test

--- tests/Integration/DSC_DefaultGatewayAddress.Integration.Tests.ps1
+++ tests/Integration/DSC_DefaultGatewayAddress.Integration.Tests.ps1
@@ -1,65 +1,88 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_DefaultGatewayAddress'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
-}
-
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-
-# Begin Testing
-try
-{
-    Describe 'DefaultGatewayAddress Integration Tests' {
-        # Configure Loopback Adapter
-        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
-
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile -Verbose -ErrorAction Stop
-
-        Describe "$($script:dscResourceName)_Integration" {
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force
-                } | Should -Not -Throw
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
-
-            It 'Should have set the resource and all the parameters should match' {
-                $current = Get-DscConfiguration   | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-                $current.InterfaceAlias           | Should -Be $TestDefaultGatewayAddress.InterfaceAlias
-                $current.AddressFamily            | Should -Be $TestDefaultGatewayAddress.AddressFamily
-                $current.Address                  | Should -Be $TestDefaultGatewayAddress.Address
-            }
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
         }
     }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'DefaultGatewayAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 }
-finally
-{
+
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'DefaultGatewayAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    . $configFile
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+    # Configure Loopback Adapter
+    New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+}
+
+AfterAll {
     # Remove Loopback Adapter
     Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
 
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+
     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
+Describe "$($script:dscResourceName)_Integration" {
+    It 'Should compile and apply the MOF without throwing' {
+        {
+            & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
+            Start-DscConfiguration `
+                -Path $TestDrive `
+                -ComputerName localhost `
+                -Wait `
+                -Verbose `
+                -Force
+        } | Should -Not -Throw
+    }
+
+    It 'Should be able to call Get-DscConfiguration without throwing' {
+        { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+    }
+
+    It 'Should have set the resource and all the parameters should match' {
+        $current = Get-DscConfiguration   | Where-Object -FilterScript {
+            $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+        }
+        $current.InterfaceAlias           | Should -Be $TestDefaultGatewayAddress.InterfaceAlias
+        $current.AddressFamily            | Should -Be $TestDefaultGatewayAddress.AddressFamily
+        $current.Address                  | Should -Be $TestDefaultGatewayAddress.Address
+    }
 }
