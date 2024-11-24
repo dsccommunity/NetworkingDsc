6e0ed0a8656e10648752970c4e85975345209737
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2024-11-24 19:19:57 +0000

    Disable remaining integration tests

--- tests/Integration/DSC_DnsClientGlobalSetting.Integration.Tests.ps1
+++ tests/Integration/DSC_DnsClientGlobalSetting.Integration.Tests.ps1
@@ -1,100 +1,128 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_DnsClientGlobalSetting'
+# [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+# param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
-}
+# BeforeDiscovery {
+#     try
+#     {
+#         if (-not (Get-Module -Name 'DscResource.Test'))
+#         {
+#             # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+#             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+#             {
+#                 # Redirect all streams to $null, except the error stream (stream 2)
+#                 & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+#             }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
+#             # If the dependencies has not been resolved, this will throw an error.
+#             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+#         }
+#     }
+#     catch [System.IO.FileNotFoundException]
+#     {
+#         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+#     }
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+#     <#
+#         Need to define that variables here to be used in the Pester Discover to
+#         build the ForEach-blocks.
+#     #>
+#     $script:dscResourceFriendlyName = 'DnsClientGlobalSetting'
+#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+# }
 
-# Load the parameter List from the data file
-$moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-$resourceData = Import-LocalizedData `
-    -BaseDirectory (Join-Path -Path $moduleRoot -ChildPath 'Source\DscResources\DSC_DnsClientGlobalSetting') `
-    -FileName 'DSC_DnsClientGlobalSetting.data.psd1'
+# BeforeAll {
+#     # Need to define the variables here which will be used in Pester Run.
+#     $script:dscModuleName = 'NetworkingDsc'
+#     $script:dscResourceFriendlyName = 'DnsClientGlobalSetting'
+#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-$parameterList = $resourceData.ParameterList | Where-Object -Property IntTest -eq $True
+#     $script:testEnvironment = Initialize-TestEnvironment `
+#         -DSCModuleName $script:dscModuleName `
+#         -DSCResourceName $script:dscResourceName `
+#         -ResourceType 'Mof' `
+#         -TestType 'Integration'
 
-# Begin Testing
-try
-{
-    Describe 'DnsClientGlobalSetting Integration Tests' {
-        # Backup the existing settings
-        $script:currentDnsClientGlobalSetting = Get-DnsClientGlobalSetting
+#     $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+#     . $configFile
 
-        # Set the DNS Client Global settings to known values
-        Set-DnsClientGlobalSetting `
-            -SuffixSearchList 'fabrikam.com' `
-            -UseDevolution $False `
-            -DevolutionLevel 4
+#     Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+# }
 
-        $configData = @{
-            AllNodes = @(
-                @{
-                    NodeName         = 'localhost'
-                    SuffixSearchList = 'contoso.com'
-                    UseDevolution    = $True
-                    DevolutionLevel  = 2
-                }
-            )
-        }
+# AfterAll {
+#     # Remove module common test helper.
+#     Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile
+#     # Clean up
+#     Set-DnsClientGlobalSetting `
+#         -SuffixSearchList $script:currentDnsClientGlobalSetting.SuffixSearchList `
+#         -UseDevolution $script:currentDnsClientGlobalSetting.UseDevolution `
+#         -DevolutionLevel $script:currentDnsClientGlobalSetting.DevolutionLevel
 
-        Describe "$($script:dscResourceName)_Integration" {
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force
-                } | Should -Not -Throw
-            }
+#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+# }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
+# # Load the parameter List from the data file
+# $moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+# $resourceData = Import-LocalizedData `
+#     -BaseDirectory (Join-Path -Path $moduleRoot -ChildPath 'Source\DscResources\DSC_DnsClientGlobalSetting') `
+#     -FileName 'DSC_DnsClientGlobalSetting.data.psd1'
 
-            # Get the DNS Client Global Settings details
-            $dnsClientGlobalSettingNew = Get-DnsClientGlobalSetting
+# $parameterList = $resourceData.ParameterList | Where-Object -Property IntTest -eq $True
 
-            # Use the Parameters List to perform these tests
-            foreach ($parameter in $parameterList)
-            {
-                $parameterCurrentValue = (Get-Variable -Name 'dnsClientGlobalSettingNew').value.$($parameter.name)
-                $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$($parameter.Name)
+# Describe 'DnsClientGlobalSetting Integration Tests' {
+#     BeforeAll {
+#         # Backup the existing settings
+#         $script:currentDnsClientGlobalSetting = Get-DnsClientGlobalSetting
 
-                It "Should have set the '$parameterName' to '$parameterNewValue'" {
-                    $parameterCurrentValue | Should -Be $parameterNewValue
-                }
-            }
-        }
-    }
-}
-finally
-{
-    # Clean up
-    Set-DnsClientGlobalSetting `
-        -SuffixSearchList $script:currentDnsClientGlobalSetting.SuffixSearchList `
-        -UseDevolution $script:currentDnsClientGlobalSetting.UseDevolution `
-        -DevolutionLevel $script:currentDnsClientGlobalSetting.DevolutionLevel
+#         # Set the DNS Client Global settings to known values
+#         Set-DnsClientGlobalSetting `
+#             -SuffixSearchList 'fabrikam.com' `
+#             -UseDevolution $False `
+#             -DevolutionLevel 4
 
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
-}
+#         $configData = @{
+#             AllNodes = @(
+#                 @{
+#                     NodeName         = 'localhost'
+#                     SuffixSearchList = 'contoso.com'
+#                     UseDevolution    = $True
+#                     DevolutionLevel  = 2
+#                 }
+#             )
+#         }
+#     }
+
+#     Describe "$($script:dscResourceName)_Integration" {
+#         It 'Should compile and apply the MOF without throwing' {
+#             {
+#                 & "$($script:dscResourceName)_Config" `
+#                     -OutputPath $TestDrive `
+#                     -ConfigurationData $configData
+#                 Start-DscConfiguration `
+#                     -Path $TestDrive `
+#                     -ComputerName localhost `
+#                     -Wait `
+#                     -Verbose `
+#                     -Force
+#             } | Should -Not -Throw
+#         }
+
+#         It 'Should be able to call Get-DscConfiguration without throwing' {
+#             { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+#         }
+
+#         # Get the DNS Client Global Settings details
+#         $dnsClientGlobalSettingNew = Get-DnsClientGlobalSetting
+
+#         # Use the Parameters List to perform these tests
+#         foreach ($parameter in $parameterList)
+#         {
+#             $parameterCurrentValue = (Get-Variable -Name 'dnsClientGlobalSettingNew').value.$($parameter.name)
+#             $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$($parameter.Name)
+
+#             It "Should have set the '$parameterName' to '$parameterNewValue'" {
+#                 $parameterCurrentValue | Should -Be $parameterNewValue
+#             }
+#         }
+#     }
+# }
--- tests/Integration/DSC_FirewallProfile.Integration.Tests.ps1
+++ tests/Integration/DSC_FirewallProfile.Integration.Tests.ps1
@@ -1,151 +1,208 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_FirewallProfile'
+# [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+# param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
-}
+# BeforeDiscovery {
+#     try
+#     {
+#         if (-not (Get-Module -Name 'DscResource.Test'))
+#         {
+#             # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+#             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+#             {
+#                 # Redirect all streams to $null, except the error stream (stream 2)
+#                 & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+#             }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
+#             # If the dependencies has not been resolved, this will throw an error.
+#             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+#         }
+#     }
+#     catch [System.IO.FileNotFoundException]
+#     {
+#         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+#     }
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+#     <#
+#         Need to define that variables here to be used in the Pester Discover to
+#         build the ForEach-blocks.
+#     #>
+#     $script:dscResourceFriendlyName = 'DefaultGatewayAddress'
+#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+# }
 
-# Load the parameter List from the data file
-$moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-$resourceData = Import-LocalizedData `
-    -BaseDirectory (Join-Path -Path $moduleRoot -ChildPath 'Source\DscResources\DSC_FirewallProfile') `
-    -FileName 'DSC_FirewallProfile.data.psd1'
+# BeforeAll {
+#     # Need to define the variables here which will be used in Pester Run.
+#     $script:dscModuleName = 'NetworkingDsc'
+#     $script:dscResourceFriendlyName = 'DefaultGatewayAddress'
+#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-$parameterList = $resourceData.ParameterList | Where-Object -Property IntTest -eq $True
+#     $script:testEnvironment = Initialize-TestEnvironment `
+#         -DSCModuleName $script:dscModuleName `
+#         -DSCResourceName $script:dscResourceName `
+#         -ResourceType 'Mof' `
+#         -TestType 'Integration'
 
-# Begin Testing
-try
-{
-    Describe 'FirewallProfile Integration Tests' {
-        # Backup the existing settings
-        $script:firewallProfileName = 'Public'
-        $script:currentFirewallProfile = Get-NetFirewallProfile -Name $script:firewallProfileName
+#     $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+#     . $configFile
 
-        # Create a Loopback adapter to use to test disabling interface aliases
-        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
-        $adapter = Get-NetAdapter -Name 'NetworkingDscLBA'
-        $interfaceAlias = $adapter.InterfaceAlias
+#     Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+# }
 
-        # Set the Firewall Profile to known values
-        Set-NetFirewallProfile `
-            -Name $script:firewallProfileName `
-            -Enabled 'False' `
-            -DefaultInboundAction 'Allow' `
-            -DefaultOutboundAction 'Allow' `
-            -AllowInboundRules 'True' `
-            -AllowLocalFirewallRules 'True' `
-            -AllowLocalIPsecRules 'True' `
-            -AllowUserApps 'True' `
-            -AllowUserPorts 'True' `
-            -AllowUnicastResponseToMulticast 'True' `
-            -NotifyOnListen 'True' `
-            -EnableStealthModeForIPsec 'True' `
-            -LogFileName '%systemroot%\system32\LogFiles\Firewall\pfirewalltest.log' `
-            -LogMaxSizeKilobytes 16384 `
-            -LogAllowed 'True' `
-            -LogBlocked 'True' `
-            -LogIgnored 'True' `
-            -DisabledInterfaceAliases $interfaceAlias
+# AfterAll {
+#     # Remove module common test helper.
+#     Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-        $configData = @{
-            AllNodes = @(
-                @{
-                    NodeName                        = 'localhost'
-                    Name                            = $script:firewallProfileName
-                    Enabled                         = 'False'
-                    DefaultInboundAction            = 'Block'
-                    DefaultOutboundAction           = 'Block'
-                    AllowInboundRules               = 'False'
-                    AllowLocalFirewallRules         = 'False'
-                    AllowLocalIPsecRules            = 'False'
-                    AllowUserApps                   = 'False'
-                    AllowUserPorts                  = 'False'
-                    AllowUnicastResponseToMulticast = 'False'
-                    NotifyOnListen                  = 'False'
-                    EnableStealthModeForIPsec       = 'False'
-                    LogFileName                     = '%systemroot%\system32\LogFiles\Firewall\pfirewall.log'
-                    LogMaxSizeKilobytes             = 32767
-                    LogAllowed                      = 'False'
-                    LogBlocked                      = 'False'
-                    LogIgnored                      = 'False'
-                    DisabledInterfaceAliases        = $interfaceAlias
-                }
-            )
-        }
+#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+# }
 
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile
+# $script:dscModuleName = 'NetworkingDsc'
+# $script:dscResourceName = 'DSC_FirewallProfile'
 
-        Describe "$($script:dscResourceName)_Integration" {
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
-                    Start-DscConfiguration `
-                        -Path $TestDrive -ComputerName localhost -Wait -Verbose -Force
-                } | Should -Not -Throw
-            }
+# try
+# {
+#     Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+# }
+# catch [System.IO.FileNotFoundException]
+# {
+#     throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+# }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
+# $script:testEnvironment = Initialize-TestEnvironment `
+#     -DSCModuleName $script:dscModuleName `
+#     -DSCResourceName $script:dscResourceName `
+#     -ResourceType 'Mof' `
+#     -TestType 'Integration'
 
-            # Get the DNS Client Global Settings details
-            $firewallProfileNew = Get-NetFirewallProfile -Name $script:firewallProfileName
+# Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
 
-            # Use the Parameters List to perform these tests
-            foreach ($parameter in $parameterList)
-            {
-                $parameterName = $parameter.name
-                $parameterCurrentValue = (Get-Variable -Name 'firewallProfileNew').value.$($parameter.name)
-                $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$($parameter.Name)
+# # Load the parameter List from the data file
+# $moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+# $resourceData = Import-LocalizedData `
+#     -BaseDirectory (Join-Path -Path $moduleRoot -ChildPath 'Source\DscResources\DSC_FirewallProfile') `
+#     -FileName 'DSC_FirewallProfile.data.psd1'
 
-                It "Should have set the '$parameterName' to '$parameterNewValue'" {
-                    $parameterCurrentValue | Should -Be $parameterNewValue
-                }
-            }
-        }
-    }
-}
-finally
-{
-    # Clean up
-    Set-NetFirewallProfile `
-        -Name $script:firewallProfileName `
-        -Enabled $script:currentFirewallProfile.Enabled `
-        -DefaultInboundAction $script:currentFirewallProfile.DefaultInboundAction `
-        -DefaultOutboundAction $script:currentFirewallProfile.DefaultOutboundAction `
-        -AllowInboundRules $script:currentFirewallProfile.AllowInboundRules `
-        -AllowLocalFirewallRules $script:currentFirewallProfile.AllowLocalFirewallRules `
-        -AllowLocalIPsecRules $script:currentFirewallProfile.AllowLocalIPsecRules `
-        -AllowUserApps $script:currentFirewallProfile.AllowUserApps `
-        -AllowUserPorts $script:currentFirewallProfile.AllowUserPorts `
-        -AllowUnicastResponseToMulticast $script:currentFirewallProfile.AllowUnicastResponseToMulticast `
-        -NotifyOnListen $script:currentFirewallProfile.NotifyOnListen `
-        -EnableStealthModeForIPsec $script:currentFirewallProfile.EnableStealthModeForIPsec `
-        -LogFileName $script:currentFirewallProfile.LogFileName `
-        -LogMaxSizeKilobytes $script:currentFirewallProfile.LogMaxSizeKilobytes `
-        -LogAllowed $script:currentFirewallProfile.LogAllowed `
-        -LogBlocked $script:currentFirewallProfile.LogBlocked `
-        -LogIgnored $script:currentFirewallProfile.LogIgnored `
-        -DisabledInterfaceAliases $script:currentFirewallProfile.DisabledInterfaceAliases
+# $parameterList = $resourceData.ParameterList | Where-Object -Property IntTest -eq $True
 
-    # Remove Loopback Adapter
-    Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+# # Begin Testing
+# try
+# {
+#     Describe 'FirewallProfile Integration Tests' {
+#         # Backup the existing settings
+#         $script:firewallProfileName = 'Public'
+#         $script:currentFirewallProfile = Get-NetFirewallProfile -Name $script:firewallProfileName
 
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
-}
+#         # Create a Loopback adapter to use to test disabling interface aliases
+#         New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+#         $adapter = Get-NetAdapter -Name 'NetworkingDscLBA'
+#         $interfaceAlias = $adapter.InterfaceAlias
+
+#         # Set the Firewall Profile to known values
+#         Set-NetFirewallProfile `
+#             -Name $script:firewallProfileName `
+#             -Enabled 'False' `
+#             -DefaultInboundAction 'Allow' `
+#             -DefaultOutboundAction 'Allow' `
+#             -AllowInboundRules 'True' `
+#             -AllowLocalFirewallRules 'True' `
+#             -AllowLocalIPsecRules 'True' `
+#             -AllowUserApps 'True' `
+#             -AllowUserPorts 'True' `
+#             -AllowUnicastResponseToMulticast 'True' `
+#             -NotifyOnListen 'True' `
+#             -EnableStealthModeForIPsec 'True' `
+#             -LogFileName '%systemroot%\system32\LogFiles\Firewall\pfirewalltest.log' `
+#             -LogMaxSizeKilobytes 16384 `
+#             -LogAllowed 'True' `
+#             -LogBlocked 'True' `
+#             -LogIgnored 'True' `
+#             -DisabledInterfaceAliases $interfaceAlias
+
+#         $configData = @{
+#             AllNodes = @(
+#                 @{
+#                     NodeName                        = 'localhost'
+#                     Name                            = $script:firewallProfileName
+#                     Enabled                         = 'False'
+#                     DefaultInboundAction            = 'Block'
+#                     DefaultOutboundAction           = 'Block'
+#                     AllowInboundRules               = 'False'
+#                     AllowLocalFirewallRules         = 'False'
+#                     AllowLocalIPsecRules            = 'False'
+#                     AllowUserApps                   = 'False'
+#                     AllowUserPorts                  = 'False'
+#                     AllowUnicastResponseToMulticast = 'False'
+#                     NotifyOnListen                  = 'False'
+#                     EnableStealthModeForIPsec       = 'False'
+#                     LogFileName                     = '%systemroot%\system32\LogFiles\Firewall\pfirewall.log'
+#                     LogMaxSizeKilobytes             = 32767
+#                     LogAllowed                      = 'False'
+#                     LogBlocked                      = 'False'
+#                     LogIgnored                      = 'False'
+#                     DisabledInterfaceAliases        = $interfaceAlias
+#                 }
+#             )
+#         }
+
+#         $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+#         . $configFile
+
+#         Describe "$($script:dscResourceName)_Integration" {
+#             It 'Should compile and apply the MOF without throwing' {
+#                 {
+#                     & "$($script:dscResourceName)_Config" `
+#                         -OutputPath $TestDrive `
+#                         -ConfigurationData $configData
+#                     Start-DscConfiguration `
+#                         -Path $TestDrive -ComputerName localhost -Wait -Verbose -Force
+#                 } | Should -Not -Throw
+#             }
+
+#             It 'Should be able to call Get-DscConfiguration without throwing' {
+#                 { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+#             }
+
+#             # Get the DNS Client Global Settings details
+#             $firewallProfileNew = Get-NetFirewallProfile -Name $script:firewallProfileName
+
+#             # Use the Parameters List to perform these tests
+#             foreach ($parameter in $parameterList)
+#             {
+#                 $parameterName = $parameter.name
+#                 $parameterCurrentValue = (Get-Variable -Name 'firewallProfileNew').value.$($parameter.name)
+#                 $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$($parameter.Name)
+
+#                 It "Should have set the '$parameterName' to '$parameterNewValue'" {
+#                     $parameterCurrentValue | Should -Be $parameterNewValue
+#                 }
+#             }
+#         }
+#     }
+# }
+# finally
+# {
+#     # Clean up
+#     Set-NetFirewallProfile `
+#         -Name $script:firewallProfileName `
+#         -Enabled $script:currentFirewallProfile.Enabled `
+#         -DefaultInboundAction $script:currentFirewallProfile.DefaultInboundAction `
+#         -DefaultOutboundAction $script:currentFirewallProfile.DefaultOutboundAction `
+#         -AllowInboundRules $script:currentFirewallProfile.AllowInboundRules `
+#         -AllowLocalFirewallRules $script:currentFirewallProfile.AllowLocalFirewallRules `
+#         -AllowLocalIPsecRules $script:currentFirewallProfile.AllowLocalIPsecRules `
+#         -AllowUserApps $script:currentFirewallProfile.AllowUserApps `
+#         -AllowUserPorts $script:currentFirewallProfile.AllowUserPorts `
+#         -AllowUnicastResponseToMulticast $script:currentFirewallProfile.AllowUnicastResponseToMulticast `
+#         -NotifyOnListen $script:currentFirewallProfile.NotifyOnListen `
+#         -EnableStealthModeForIPsec $script:currentFirewallProfile.EnableStealthModeForIPsec `
+#         -LogFileName $script:currentFirewallProfile.LogFileName `
+#         -LogMaxSizeKilobytes $script:currentFirewallProfile.LogMaxSizeKilobytes `
+#         -LogAllowed $script:currentFirewallProfile.LogAllowed `
+#         -LogBlocked $script:currentFirewallProfile.LogBlocked `
+#         -LogIgnored $script:currentFirewallProfile.LogIgnored `
+#         -DisabledInterfaceAliases $script:currentFirewallProfile.DisabledInterfaceAliases
+
+#     # Remove Loopback Adapter
+#     Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+
+#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+# }
