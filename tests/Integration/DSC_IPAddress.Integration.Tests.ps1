705b7213cea708eb42240cb035b7dc0d2c356773
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2024-11-24 16:32:24 +0000

    Update Integration tests

--- tests/Integration/DSC_IPAddress.Integration.Tests.ps1
+++ tests/Integration/DSC_IPAddress.Integration.Tests.ps1
@@ -1,131 +1,159 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_IPAddress'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
-}
-
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
-
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-
-# Begin Testing
-try
-{
-    Describe 'IPAddress Integration Tests' {
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile -Verbose -ErrorAction Stop
-
-        BeforeAll {
-            New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
-            New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
-        }
-
-        AfterAll {
-            Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
-            Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
-        }
-
-        Describe "$($script:dscResourceName)_Integration" {
-            Context 'When a single IP address is specified' {
-                # This is to pass to the Config
-                $configData = @{
-                    AllNodes = @(
-                        @{
-                            NodeName       = 'localhost'
-                            InterfaceAlias = 'NetworkingDscLBA1'
-                            AddressFamily  = 'IPv4'
-                            IPAddress      = '10.11.12.13/16'
-                        }
-                    )
-                }
-
-                It 'Should compile and apply the MOF without throwing' {
-                    {
-                        & "$($script:dscResourceName)_Config" `
-                            -OutputPath $TestDrive `
-                            -ConfigurationData $configData
-
-                        Start-DscConfiguration `
-                            -Path $TestDrive `
-                            -ComputerName localhost `
-                            -Wait `
-                            -Verbose `
-                            -Force `
-                            -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-                It 'should be able to call Get-DscConfiguration without throwing' {
-                    { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-                }
-
-                It 'Should have set the resource and all the parameters should match' {
-                    $current = Get-DscConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                    }
-                    $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
-                    $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
-                    $current[0].IPAddress | Should -Be $configData.AllNodes[0].IPAddress
-                }
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
-        }
 
-        Context 'When a two IP addresses are specified' {
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'DSC_IPAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+}
+
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'DSC_IPAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    . $configFile
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+}
+
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
+
+Describe "$($script:dscResourceName)_Integration" {
+    BeforeAll {
+        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
+        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
+    }
+
+    AfterAll {
+        Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
+        Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
+    }
+
+    Context 'When a single IP address is specified' {
+        BeforeAll {
             # This is to pass to the Config
             $configData = @{
                 AllNodes = @(
                     @{
                         NodeName       = 'localhost'
-                        InterfaceAlias = 'NetworkingDscLBA2'
+                        InterfaceAlias = 'NetworkingDscLBA1'
                         AddressFamily  = 'IPv4'
-                        IPAddress      = @('10.12.13.14/16', '10.13.14.16/32')
+                        IPAddress      = '10.11.12.13/16'
                     }
                 )
             }
+        }
 
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configData
 
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force `
-                        -ErrorAction Stop
-                } | Should -Not -Throw
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
             }
-
-            It 'should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
-
-            It 'Should have set the resource and all the parameters should match' {
-                $current = Get-DscConfiguration | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-                $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
-                $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
-                $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[0]
-                $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[1]
-            }
+            $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
+            $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
+            $current[0].IPAddress | Should -Be $configData.AllNodes[0].IPAddress
         }
     }
 }
-finally
-{
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+
+Context 'When a two IP addresses are specified' {
+    BeforeAll {
+        # This is to pass to the Config
+        $configData = @{
+            AllNodes = @(
+                @{
+                    NodeName       = 'localhost'
+                    InterfaceAlias = 'NetworkingDscLBA2'
+                    AddressFamily  = 'IPv4'
+                    IPAddress      = @('10.12.13.14/16', '10.13.14.16/32')
+                }
+            )
+        }
+    }
+
+    It 'Should compile and apply the MOF without throwing' {
+        {
+            & "$($script:dscResourceName)_Config" `
+                -OutputPath $TestDrive `
+                -ConfigurationData $configData
+
+            Start-DscConfiguration `
+                -Path $TestDrive `
+                -ComputerName localhost `
+                -Wait `
+                -Verbose `
+                -Force `
+                -ErrorAction Stop
+        } | Should -Not -Throw
+    }
+
+    It 'should be able to call Get-DscConfiguration without throwing' {
+        { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+    }
+
+    It 'Should have set the resource and all the parameters should match' {
+        $current = Get-DscConfiguration | Where-Object -FilterScript {
+            $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+        }
+        $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
+        $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
+        $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[0]
+        $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[1]
+    }
 }
--- tests/Integration/DSC_WinsSetting.Integration.Tests.ps1
+++ tests/Integration/DSC_WinsSetting.Integration.Tests.ps1
@@ -1,60 +1,92 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_WinsSetting'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-# Find an adapter we can test with. It needs to be enabled and have IP enabled.
-$netAdapter = $null
-$netAdapterConfig = $null
-$netAdapterEnabled = Get-CimInstance -ClassName Win32_NetworkAdapter -Filter 'NetEnabled="True"'
-
-if (-not $netAdapterEnabled)
-{
-    Write-Verbose -Message ('There are no enabled network adapters in this system. Integration tests will be skipped.') -Verbose
-    return
-}
-
-foreach ($netAdapter in $netAdapterEnabled)
-{
-    $netAdapterConfig = $netAdapter |
-        Get-CimAssociatedInstance -ResultClassName Win32_NetworkAdapterConfiguration |
-        Where-Object -FilterScript { $_.IPEnabled -eq $True }
-
-    if ($netAdapterConfig)
+BeforeDiscovery {
+    try
     {
-        break
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
     }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'WinsSetting'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 }
 
-if (-not $netAdapterConfig)
-{
-    Write-Verbose -Message ('There are no enabled network adapters with IP enabled in this system. Integration tests will be skipped.') -Verbose
-    return
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'WinsSetting'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    . $configFile
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
 }
 
-Write-Verbose -Message ('A network adapter ({0}) was found in this system that meets requirements for integration testing.' -f $netAdapter.Name) -Verbose
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
-}
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
+Describe "$($script:dscResourceName)_Integration" {
+    BeforeAll {
+        # Find an adapter we can test with. It needs to be enabled and have IP enabled.
+        $netAdapter = $null
+        $netAdapterConfig = $null
+        $netAdapterEnabled = Get-CimInstance -ClassName Win32_NetworkAdapter -Filter 'NetEnabled="True"'
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+        if (-not $netAdapterEnabled)
+        {
+            Write-Verbose -Message ('There are no enabled network adapters in this system. Integration tests will be skipped.') -Verbose
+            return
+        }
 
-# Begin Testing
-try
-{
-    Describe 'WinsSetting Integration Tests' {
-        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile
+        foreach ($netAdapter in $netAdapterEnabled)
+        {
+            $netAdapterConfig = $netAdapter |
+                Get-CimAssociatedInstance -ResultClassName Win32_NetworkAdapterConfiguration |
+                Where-Object -FilterScript { $_.IPEnabled -eq $True }
+
+            if ($netAdapterConfig)
+            {
+                break
+            }
+        }
+
+        if (-not $netAdapterConfig)
+        {
+            Write-Verbose -Message ('There are no enabled network adapters with IP enabled in this system. Integration tests will be skipped.') -Verbose
+            return
+        }
+
+        Write-Verbose -Message ('A network adapter ({0}) was found in this system that meets requirements for integration testing.' -f $netAdapter.Name) -Verbose
 
         # Store the current WINS settings
         $enableDnsRegistryKey = Get-ItemProperty `
@@ -84,103 +116,103 @@ try
             -ClassName Win32_NetworkAdapterConfiguration `
             -MethodName EnableWins `
             -Arguments @{
-                DNSEnabledForWINSResolution = $true
-                WINSEnableLMHostsLookup     = $true
-            }
-
-        Describe "$($script:dscResourceName)_Integration" {
-            Context 'Disable all settings' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{
-                            NodeName      = 'localhost'
-                            EnableLmHosts = $false
-                            EnableDns     = $false
-                        }
-                    )
-                }
-
-                It 'Should compile and apply the MOF without throwing' {
-                    {
-                        & "$($script:dscResourceName)_Config" `
-                            -OutputPath $TestDrive `
-                            -ConfigurationData $configurationData
-
-                        Start-DscConfiguration `
-                            -Path $TestDrive `
-                            -ComputerName localhost `
-                            -Wait `
-                            -Verbose `
-                            -Force `
-                            -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-                It 'Should be able to call Get-DscConfiguration without throwing' {
-                    { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-                }
-
-                It 'Should have set the resource and all setting should match current state' {
-                    $result = Get-DscConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                    }
-                    $result.EnableLmHosts | Should -Be $false
-                    $result.EnableDns | Should -Be $false
-                }
-            }
-
-            Context 'Enable all settings' {
-                $configurationData = @{
-                    AllNodes = @(
-                        @{
-                            NodeName      = 'localhost'
-                            EnableLmHosts = $true
-                            EnableDns     = $true
-                        }
-                    )
-                }
-
-                It 'Should compile and apply the MOF without throwing' {
-                    {
-                        & "$($script:dscResourceName)_Config" `
-                            -OutputPath $TestDrive `
-                            -ConfigurationData $configurationData
-
-                        Start-DscConfiguration `
-                            -Path $TestDrive `
-                            -ComputerName localhost `
-                            -Wait `
-                            -Verbose `
-                            -Force `
-                            -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-                It 'Should be able to call Get-DscConfiguration without throwing' {
-                    { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-                }
-
-                It 'Should have set the resource and all setting should match current state' {
-                    $result = Get-DscConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                    }
-                    $result.EnableLmHosts | Should -Be $true
-                    $result.EnableDns | Should -Be $true
-                }
-            }
+            DNSEnabledForWINSResolution = $true
+            WINSEnableLMHostsLookup     = $true
         }
     }
-}
-finally
-{
-    # Restore the WINS settings
-    $null = Invoke-CimMethod `
-        -ClassName Win32_NetworkAdapterConfiguration `
-        -MethodName EnableWins `
-        -Arguments @{
+
+    AfterAll {
+        # Restore the WINS settings
+        $null = Invoke-CimMethod `
+            -ClassName Win32_NetworkAdapterConfiguration `
+            -MethodName EnableWins `
+            -Arguments @{
             DNSEnabledForWINSResolution = $currentEnableDns
             WINSEnableLMHostsLookup     = $currentEnableLmHosts
         }
+    }
 
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+    Context 'Disable all settings' {
+        BeforeAll {
+            $configurationData = @{
+                AllNodes = @(
+                    @{
+                        NodeName      = 'localhost'
+                        EnableLmHosts = $false
+                        EnableDns     = $false
+                    }
+                )
+            }
+        }
+
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configurationData
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all setting should match current state' {
+            $result = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+            }
+            $result.EnableLmHosts | Should -Be $false
+            $result.EnableDns | Should -Be $false
+        }
+    }
+
+    Context 'Enable all settings' {
+        BeforeAll {
+            $configurationData = @{
+                AllNodes = @(
+                    @{
+                        NodeName      = 'localhost'
+                        EnableLmHosts = $true
+                        EnableDns     = $true
+                    }
+                )
+            }
+        }
+
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configurationData
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all setting should match current state' {
+            $result = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+            }
+            $result.EnableLmHosts | Should -Be $true
+            $result.EnableDns | Should -Be $true
+        }
+    }
 }
