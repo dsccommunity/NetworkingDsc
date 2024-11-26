c888d5c36a3cb1109997ec0b4f84224517fe0dfc
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-11-26 15:32:30 +0000

    Enable IPAddress int test

--- tests/Integration/DSC_IPAddress.Integration.Tests.ps1
+++ tests/Integration/DSC_IPAddress.Integration.Tests.ps1
@@ -1,159 +1,171 @@
-# [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
-# param ()
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-# BeforeDiscovery {
-#     try
-#     {
-#         if (-not (Get-Module -Name 'DscResource.Test'))
-#         {
-#             # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
-#             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
-#             {
-#                 # Redirect all streams to $null, except the error stream (stream 2)
-#                 & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
-#             }
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
 
-#             # If the dependencies has not been resolved, this will throw an error.
-#             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
-#         }
-#     }
-#     catch [System.IO.FileNotFoundException]
-#     {
-#         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
-#     }
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
 
-#     <#
-#         Need to define that variables here to be used in the Pester Discover to
-#         build the ForEach-blocks.
-#     #>
-#     $script:dscResourceFriendlyName = 'IPAddress'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
-# }
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'IPAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+}
 
-# BeforeAll {
-#     # Need to define the variables here which will be used in Pester Run.
-#     $script:dscModuleName = 'NetworkingDsc'
-#     $script:dscResourceFriendlyName = 'IPAddress'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'IPAddress'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-#     $script:testEnvironment = Initialize-TestEnvironment `
-#         -DSCModuleName $script:dscModuleName `
-#         -DSCResourceName $script:dscResourceName `
-#         -ResourceType 'Mof' `
-#         -TestType 'Integration'
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
 
-#     $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-#     . $configFile
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+}
 
-#     Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-# }
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-# AfterAll {
-#     # Remove module common test helper.
-#     Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
 
-#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
-# }
+Describe 'HostsFile Integration Tests' {
+    BeforeAll {
+        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
+        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
 
-# Describe "$($script:dscResourceName)_Integration" {
-#     BeforeAll {
-#         New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
-#         New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
-#     }
+        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+        . $configFile
+    }
 
-#     AfterAll {
-#         Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
-#         Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
-#     }
+    AfterAll {
+        Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA1'
+        Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA2'
+    }
+    
+    Describe "$($script:dscResourceName)_Integration" {
+        Context 'When a single IP address is specified' {
+            BeforeAll {
+                # This is to pass to the Config
+                $configData = @{
+                    AllNodes = @(
+                        @{
+                            NodeName       = 'localhost'
+                            InterfaceAlias = 'NetworkingDscLBA1'
+                            AddressFamily  = 'IPv4'
+                            IPAddress      = '10.11.12.13/16'
+                        }
+                    )
+                }
+            }
 
-#     Context 'When a single IP address is specified' {
-#         BeforeAll {
-#             # This is to pass to the Config
-#             $configData = @{
-#                 AllNodes = @(
-#                     @{
-#                         NodeName       = 'localhost'
-#                         InterfaceAlias = 'NetworkingDscLBA1'
-#                         AddressFamily  = 'IPv4'
-#                         IPAddress      = '10.11.12.13/16'
-#                     }
-#                 )
-#             }
-#         }
+            AfterEach {
+                Wait-ForIdleLcm
+            }
 
-#         It 'Should compile and apply the MOF without throwing' {
-#             {
-#                 & "$($script:dscResourceName)_Config" `
-#                     -OutputPath $TestDrive `
-#                     -ConfigurationData $configData
+            It 'Should compile and apply the MOF without throwing' {
+                {
+                    & "$($script:dscResourceName)_Config" `
+                        -OutputPath $TestDrive `
+                        -ConfigurationData $configData
 
-#                 Start-DscConfiguration `
-#                     -Path $TestDrive `
-#                     -ComputerName localhost `
-#                     -Wait `
-#                     -Verbose `
-#                     -Force `
-#                     -ErrorAction Stop
-#             } | Should -Not -Throw
-#         }
+                    Start-DscConfiguration `
+                        -Path $TestDrive `
+                        -ComputerName localhost `
+                        -Wait `
+                        -Verbose `
+                        -Force `
+                        -ErrorAction Stop
+                } | Should -Not -Throw
+            }
 
-#         It 'should be able to call Get-DscConfiguration without throwing' {
-#             { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-#         }
+            It 'should be able to call Get-DscConfiguration without throwing' {
+                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+            }
 
-#         It 'Should have set the resource and all the parameters should match' {
-#             $current = Get-DscConfiguration | Where-Object -FilterScript {
-#                 $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-#             }
-#             $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
-#             $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
-#             $current[0].IPAddress | Should -Be $configData.AllNodes[0].IPAddress
-#         }
-#     }
-# }
+            It 'Should have set the resource and all the parameters should match' {
+                $current = Get-DscConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+                }
 
-# Context 'When a two IP addresses are specified' {
-#     BeforeAll {
-#         # This is to pass to the Config
-#         $configData = @{
-#             AllNodes = @(
-#                 @{
-#                     NodeName       = 'localhost'
-#                     InterfaceAlias = 'NetworkingDscLBA2'
-#                     AddressFamily  = 'IPv4'
-#                     IPAddress      = @('10.12.13.14/16', '10.13.14.16/32')
-#                 }
-#             )
-#         }
-#     }
+                $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
+                $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
+                $current[0].IPAddress | Should -Be $configData.AllNodes[0].IPAddress
+            }
+        }
 
-#     It 'Should compile and apply the MOF without throwing' {
-#         {
-#             & "$($script:dscResourceName)_Config" `
-#                 -OutputPath $TestDrive `
-#                 -ConfigurationData $configData
+        Context 'When a two IP addresses are specified' {
+            BeforeAll {
+                # This is to pass to the Config
+                $configData = @{
+                    AllNodes = @(
+                        @{
+                            NodeName       = 'localhost'
+                            InterfaceAlias = 'NetworkingDscLBA2'
+                            AddressFamily  = 'IPv4'
+                            IPAddress      = @('10.12.13.14/16', '10.13.14.16/32')
+                        }
+                    )
+                }
+            }
 
-#             Start-DscConfiguration `
-#                 -Path $TestDrive `
-#                 -ComputerName localhost `
-#                 -Wait `
-#                 -Verbose `
-#                 -Force `
-#                 -ErrorAction Stop
-#         } | Should -Not -Throw
-#     }
+            AfterEach {
+                Wait-ForIdleLcm
+            }
 
-#     It 'should be able to call Get-DscConfiguration without throwing' {
-#         { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-#     }
+            It 'Should compile and apply the MOF without throwing' {
+                {
+                    & "$($script:dscResourceName)_Config" `
+                        -OutputPath $TestDrive `
+                        -ConfigurationData $configData
 
-#     It 'Should have set the resource and all the parameters should match' {
-#         $current = Get-DscConfiguration | Where-Object -FilterScript {
-#             $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-#         }
-#         $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
-#         $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
-#         $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[0]
-#         $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[1]
-#     }
-# }
+                    Start-DscConfiguration `
+                        -Path $TestDrive `
+                        -ComputerName localhost `
+                        -Wait `
+                        -Verbose `
+                        -Force `
+                        -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+            It 'should be able to call Get-DscConfiguration without throwing' {
+                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+            }
+
+            It 'Should have set the resource and all the parameters should match' {
+                $current = Get-DscConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+                }
+
+                $current[0].InterfaceAlias | Should -Be $configData.AllNodes[0].InterfaceAlias
+                $current[0].AddressFamily | Should -Be $configData.AllNodes[0].AddressFamily
+                $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[0]
+                $current[0].IPAddress | Should -Contain $configData.AllNodes[0].IPAddress[1]
+            }
+        }
+    }
+}
