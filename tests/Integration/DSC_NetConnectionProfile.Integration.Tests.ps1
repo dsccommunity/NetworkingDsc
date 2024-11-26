0df5fea78ac16a86f8de260c974439b3d4d20c63
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-11-26 16:55:21 +0000

    Enable netconnectionprofile int tests

--- tests/Integration/DSC_NetConnectionProfile.Integration.Tests.ps1
+++ tests/Integration/DSC_NetConnectionProfile.Integration.Tests.ps1
@@ -1,81 +1,83 @@
-# [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
-# param ()
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-# BeforeDiscovery {
-#     try
-#     {
-#         if (-not (Get-Module -Name 'DscResource.Test'))
-#         {
-#             # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
-#             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
-#             {
-#                 # Redirect all streams to $null, except the error stream (stream 2)
-#                 & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
-#             }
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
 
-#             # If the dependencies has not been resolved, this will throw an error.
-#             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
-#         }
-#     }
-#     catch [System.IO.FileNotFoundException]
-#     {
-#         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
-#     }
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
 
-#     <#
-#         Need to define that variables here to be used in the Pester Discover to
-#         build the ForEach-blocks.
-#     #>
-#     $script:dscResourceFriendlyName = 'NetConnectionProfile'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
-# }
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'NetConnectionProfile'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+}
 
-# BeforeAll {
-#     # Need to define the variables here which will be used in Pester Run.
-#     $script:dscModuleName = 'NetworkingDsc'
-#     $script:dscResourceFriendlyName = 'NetConnectionProfile'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'NetConnectionProfile'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-#     $script:testEnvironment = Initialize-TestEnvironment `
-#         -DSCModuleName $script:dscModuleName `
-#         -DSCResourceName $script:dscResourceName `
-#         -ResourceType 'Mof' `
-#         -TestType 'Integration'
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
 
-#     $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-#     . $configFile
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+}
 
-#     Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-# }
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-# AfterAll {
-#     # Remove module common test helper.
-#     Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
 
-#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
-# }
+Describe 'NetConnectionProfile Integration Tests' {
+    BeforeAll {
+        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+        . $configFile
+    }
 
-# Describe 'NetConnectionProfile Integration Tests' {
-#     Describe "$($script:dscResourceName)_Integration" {
-#         It 'Should compile and apply the MOF without throwing' {
-#             {
-#                 & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
-#                 Start-DscConfiguration -Path $TestDrive -ComputerName localhost -Wait -Verbose -Force
-#             } | Should -Not -Throw
-#         }
+    Describe "$($script:dscResourceName)_Integration" {
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
+                Start-DscConfiguration -Path $TestDrive -ComputerName localhost -Wait -Verbose -Force
+            } | Should -Not -Throw
+        }
 
-#         It 'Should be able to call Get-DscConfiguration without throwing' {
-#             { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-#         }
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
 
-#         It 'Should have set the resource and all the parameters should match' {
-#             $current = Get-DscConfiguration | Where-Object { $_.ConfigurationName -eq "$($script:dscResourceName)_Config" }
-#             $rule.InterfaceAlias   | Should -Be $current.InterfaceAlias
-#             $rule.NetworkCategory  | Should -Be $current.NetworkCategory
-#             $rule.IPv4Connectivity | Should -Be $current.IPv4Connectivity
-#             $rule.IPv6Connectivity | Should -Be $current.IPv6Connectivity
-#             $rule.Address          | Should -Be $current.Address
-#             $rule.AddressFamily    | Should -Be $current.AddressFamily
-#         }
-#     }
-# }
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object { $_.ConfigurationName -eq "$($script:dscResourceName)_Config" }
+            $rule.InterfaceAlias   | Should -Be $current.InterfaceAlias
+            $rule.NetworkCategory  | Should -Be $current.NetworkCategory
+            $rule.IPv4Connectivity | Should -Be $current.IPv4Connectivity
+            $rule.IPv6Connectivity | Should -Be $current.IPv6Connectivity
+            $rule.Address          | Should -Be $current.Address
+            $rule.AddressFamily    | Should -Be $current.AddressFamily
+        }
+    }
+}
