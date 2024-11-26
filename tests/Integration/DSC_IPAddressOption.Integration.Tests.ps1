a2454be8213c07923a7956d29150b0727454abb9
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-11-26 15:37:43 +0000

    Enable IPAddressOption int test

--- tests/Integration/DSC_IPAddressOption.Integration.Tests.ps1
+++ tests/Integration/DSC_IPAddressOption.Integration.Tests.ps1
@@ -1,98 +1,102 @@
-# [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
-# param ()
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-# BeforeDiscovery {
-#     try
-#     {
-#         if (-not (Get-Module -Name 'DscResource.Test'))
-#         {
-#             # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
-#             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
-#             {
-#                 # Redirect all streams to $null, except the error stream (stream 2)
-#                 & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
-#             }
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
 
-#             # If the dependencies has not been resolved, this will throw an error.
-#             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
-#         }
-#     }
-#     catch [System.IO.FileNotFoundException]
-#     {
-#         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
-#     }
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
 
-#     <#
-#         Need to define that variables here to be used in the Pester Discover to
-#         build the ForEach-blocks.
-#     #>
-#     $script:dscResourceFriendlyName = 'IPAddressOption'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
-# }
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'IPAddressOption'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+}
 
-# BeforeAll {
-#     # Need to define the variables here which will be used in Pester Run.
-#     $script:dscModuleName = 'NetworkingDsc'
-#     $script:dscResourceFriendlyName = 'IPAddressOption'
-#     $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'IPAddressOption'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 
-#     $script:testEnvironment = Initialize-TestEnvironment `
-#         -DSCModuleName $script:dscModuleName `
-#         -DSCResourceName $script:dscResourceName `
-#         -ResourceType 'Mof' `
-#         -TestType 'Integration'
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+}
 
-#     $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-#     . $configFile
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-#     Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
-# }
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
 
-# AfterAll {
-#     # Remove module common test helper.
-#     Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
+Describe 'IPAddressOption Integration Tests' {
+    BeforeAll {
+        New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
 
-#     Restore-TestEnvironment -TestEnvironment $script:testEnvironment
-# }
+        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+        . $configFile
+    }
 
-# Describe 'IPAddressOption Integration Tests' {
-#     BeforeAll {
-#         New-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
-#     }
+    AfterAll {
+        # Remove Loopback Adapter
+        Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
+    }
 
-#     AfterAll {
-#         # Remove Loopback Adapter
-#         Remove-IntegrationLoopbackAdapter -AdapterName 'NetworkingDscLBA'
-#     }
+    Describe "$($script:dscResourceName)_Integration" {
+        AfterEach {
+            Wait-ForIdleLcm
+        }
 
-#     Describe "$($script:dscResourceName)_Integration" {
-#         It 'Should compile and apply the MOF without throwing' {
-#             {
-#                 & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" -OutputPath $TestDrive
 
-#                 Start-DscConfiguration `
-#                     -Path $TestDrive `
-#                     -ComputerName localhost `
-#                     -Wait `
-#                     -Verbose `
-#                     -Force `
-#                     -ErrorAction Stop
-#             } | Should -Not -Throw
-#         }
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
 
-#         It 'should be able to call Get-DscConfiguration without throwing' {
-#             { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-#         }
+        It 'should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
 
-#         It 'Should have set the resource and all the parameters should match' {
-#             $current = Get-DscConfiguration | Where-Object -FilterScript {
-#                 $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-#             }
-#             $current[0].InterfaceAlias | Should -Be $TestIPAddress.InterfaceAlias
-#             $current[0].AddressFamily  | Should -Be $TestIPAddress.AddressFamily
-#             $current[0].IPAddress      | Should -Be $TestIPAddress.IPAddress
-#             $current[1].IPAddress      | Should -Be $TestIPAddressOption.IPAddress
-#             $current[1].SkipAsSource   | Should -Be $TestIPAddressOption.SkipAsSource
-#         }
-#     }
-# }
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+            }
+            
+            $current[0].InterfaceAlias | Should -Be $TestIPAddress.InterfaceAlias
+            $current[0].AddressFamily  | Should -Be $TestIPAddress.AddressFamily
+            $current[0].IPAddress      | Should -Be $TestIPAddress.IPAddress
+            $current[1].IPAddress      | Should -Be $TestIPAddressOption.IPAddress
+            $current[1].SkipAsSource   | Should -Be $TestIPAddressOption.SkipAsSource
+        }
+    }
+}
