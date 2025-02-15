e70f6573aaa9a658d9c5e8d6d0daf99c5c9738de
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-02-15 16:48:43 +0000

    Get-InvalidArgumentRecord is from DscResource.Test

--- tests/TestHelpers/CommonTestHelper.psm1
+++ tests/TestHelpers/CommonTestHelper.psm1
@@ -1,40 +1,5 @@
 <#
     .SYNOPSIS
-        Returns an invalid argument exception object
-
-    .PARAMETER Message
-        The message explaining why this error is being thrown
-
-    .PARAMETER ArgumentName
-        The name of the invalid argument that is causing this error to be thrown
-#>
-function Get-InvalidArgumentRecord
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $Message,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $ArgumentName
-    )
-
-    $argumentException = New-Object -TypeName 'ArgumentException' -ArgumentList @( $Message,
-        $ArgumentName )
-    $newObjectParams = @{
-        TypeName     = 'System.Management.Automation.ErrorRecord'
-        ArgumentList = @( $argumentException, $ArgumentName, 'InvalidArgument', $null )
-    }
-    return New-Object @newObjectParams
-}
-
-<#
-    .SYNOPSIS
         Determines if Network Team integration tests can be executed.
 
     .PARAMETER NetworkAdapters
@@ -153,7 +118,6 @@ function Remove-IntegrationLoopbackAdapter
 } # function Remove-IntegrationLoopbackAdapter
 
 Export-ModuleMember -Function `
-    Get-InvalidArgumentRecord, `
     Test-NetworkTeamIntegrationEnvironment, `
     New-IntegrationLoopbackAdapter, `
     Remove-IntegrationLoopbackAdapter
