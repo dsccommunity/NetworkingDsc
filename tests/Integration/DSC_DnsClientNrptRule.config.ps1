3c1dae5aec1969206b507b192533251c8462e8c3
Alexandre JARDON <28548335+webalexeu@users.noreply.github.com>
2025-03-17 22:26:20 +0100

    Switch Nrpt integration tests to Pester 5

--- tests/Integration/DSC_DnsClientNrptRule.config.ps1
+++ tests/Integration/DSC_DnsClientNrptRule.config.ps1
@@ -12,7 +12,6 @@ configuration DSC_DnsClientNrptRule_Config {
         DnsClientNrptRule Integration_Test {
             Name         = $Node.Name
             Namespace    = $Node.Namespace
-            NameEncoding = $Node.NameEncoding
             NameServers  = $Node.NameServers
         }
     }
