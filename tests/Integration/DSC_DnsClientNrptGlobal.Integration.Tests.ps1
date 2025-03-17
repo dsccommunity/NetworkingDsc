a658a2fece4b0ee6dbbfde6930f9bcd594fb490f
Alexandre JARDON <28548335+webalexeu@users.noreply.github.com>
2025-03-17 22:18:27 +0100

    Switch Nrpt integration tests to Pester 5

--- tests/Integration/DSC_DnsClientNrptGlobal.Integration.Tests.ps1
+++ tests/Integration/DSC_DnsClientNrptGlobal.Integration.Tests.ps1
@@ -1,100 +1,131 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_DnsClientNrptGlobal'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'DnsClientNrptGlobal'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 }
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'DnsClientNrptGlobal'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
 }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
 
-# Load the parameter List from the data file
-$moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-$resourceData = Import-LocalizedData `
-    -BaseDirectory (Join-Path -Path $moduleRoot -ChildPath 'Source\DscResources\DSC_DnsClientNrptGlobal') `
-    -FileName 'DSC_DnsClientNrptGlobal.data.psd1'
 
-$parameterList = $resourceData.ParameterList | Where-Object -Property IntTest -eq $True
-
-# Begin Testing
-try
-{
-    Describe 'DnsClientNrptGlobal Integration Tests' {
+Describe 'DnsClientNrptGlobal Integration Tests' {
+    BeforeAll {
         # Backup the existing settings
         $script:currentDnsClientNrptGlobal = Get-DnsClientNrptGlobal
 
         # Set the DNS Client Global settings to known values
         Set-DnsClientNrptGlobal `
-            -EnableDAForAllNetworks 'EnableAlways' `
-            -QueryPolicy 'QueryIPv6Only' `
-            -SecureNameQueryFallback 'FallbackSecure'
+            -EnableDAForAllNetworks 'Disable' `
+            -QueryPolicy 'Disable' `
+            -SecureNameQueryFallback 'Disable'
 
         $configData = @{
             AllNodes = @(
                 @{
                     NodeName                = 'localhost'
-                    EnableDAForAllNetworks  = 'DisableDA'
+                    EnableDAForAllNetworks  = 'EnableAlways'
                     QueryPolicy             = 'QueryBoth'
-                    SecureNameQueryFallback = 'FallbackPrivate'
+                    SecureNameQueryFallback = 'FallbackSecure'
                 }
             )
         }
 
         $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
         . $configFile
+    }
 
-        Describe "$($script:dscResourceName)_Integration" {
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force
-                } | Should -Not -Throw
-            }
+    AfterAll {
+        # Clean up
+        Set-DnsClientNrptGlobal `
+            -EnableDAForAllNetworks $script:currentDnsClientNrptGlobal.EnableDAForAllNetworks `
+            -QueryPolicy $script:currentDnsClientNrptGlobal.QueryPolicy `
+            -SecureNameQueryFallback $script:currentDnsClientNrptGlobal.SecureNameQueryFallback
+    }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
+    Describe "$($script:dscResourceName)_Integration" {
+        AfterEach {
+            Wait-ForIdleLcm
+        }
 
-            # Get the DNS Client Global Settings details
-            $DnsClientNrptGlobalNew = Get-DnsClientNrptGlobal
-
-            # Use the Parameters List to perform these tests
-            foreach ($parameter in $parameterList)
+        It 'Should compile and apply the MOF without throwing' {
             {
-                $parameterCurrentValue = (Get-Variable -Name 'DnsClientNrptGlobalNew').value.$($parameter.name)
-                $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$($parameter.Name)
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configData
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force
+            } | Should -Not -Throw
+        }
 
-                It "Should have set the '$parameterName' to '$parameterNewValue'" {
-                    $parameterCurrentValue | Should -Be $parameterNewValue
-                }
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        Context 'When testing each of the parameter values' {
+            BeforeAll {
+                # Get the DNS Client Global Settings details
+                $DnsClientNrptGlobalNew = Get-DnsClientNrptGlobal
+            }
+
+            It 'Should have the correct value for <_>' -ForEach @(
+                'EnableDAForAllNetworks',
+                'QueryPolicy',
+                'SecureNameQueryFallback'
+            ) {
+                $parameterCurrentValue = (Get-Variable -Name DnsClientNrptGlobalNew).value.$_
+                $parameterNewValue = (Get-Variable -Name configData).Value.AllNodes[0].$_
+
+                $parameterCurrentValue | Should -Be $parameterNewValue
             }
         }
     }
-}
-finally
-{
-    # Clean up
-    Set-DnsClientNrptGlobal `
-        -EnableDAForAllNetworks $script:currentDnsClientNrptGlobal.EnableDAForAllNetworks `
-        -QueryPolicy $script:currentDnsClientNrptGlobal.QueryPolicy `
-        -SecureNameQueryFallback $script:currentDnsClientNrptGlobal.SecureNameQueryFallback
-
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
--- tests/Integration/DSC_DnsClientNrptRule.Integration.Tests.ps1
+++ tests/Integration/DSC_DnsClientNrptRule.Integration.Tests.ps1
@@ -1,27 +1,59 @@
-$script:dscModuleName = 'NetworkingDsc'
-$script:dscResourceName = 'DSC_DnsClientNrptRule'
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Suppressing this rule because Script Analyzer does not understand Pester syntax.')]
+param ()
 
-try
-{
-    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
+    <#
+        Need to define that variables here to be used in the Pester Discover to
+        build the ForEach-blocks.
+    #>
+    $script:dscResourceFriendlyName = 'DnsClientNrptRule'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
 }
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+
+BeforeAll {
+    # Need to define the variables here which will be used in Pester Run.
+    $script:dscModuleName = 'NetworkingDsc'
+    $script:dscResourceFriendlyName = 'DnsClientNrptRule'
+    $script:dscResourceName = "DSC_$($script:dscResourceFriendlyName)"
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+
+    Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
 }
 
-$script:testEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -ResourceType 'Mof' `
-    -TestType 'Integration'
+AfterAll {
+    # Remove module common test helper.
+    Get-Module -Name 'CommonTestHelper' -All | Remove-Module -Force
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
 
-# Begin Testing
-try
-{
-    Describe 'NRPT Rule Integration Tests' {
+Describe 'DnsClientNrptRule Integration Tests' {
+    BeforeAll {
         $script:dummyRule = [PSObject] @{
             Name        = 'Server'
             Namespace   = '.contoso.com'
@@ -29,9 +61,18 @@ try
         }
 
         $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-        . $configFile -Verbose -ErrorAction Stop
+        . $configFile
+    }
 
-        Describe "$($script:dscResourceName)_Add_Integration" {
+    AfterAll {
+        # Clean up any created rules just in case the integration tests fail
+        $null = Remove-DnsClientNrptRule -Name $dummyRule.Name `
+            -Force `
+            -ErrorAction SilentlyContinue
+    }
+
+    Describe "$($script:dscResourceName)_Add_Integration" {
+        BeforeAll {
             $configData = @{
                 AllNodes = @(
                     @{
@@ -43,44 +84,50 @@ try
                     }
                 )
             }
-
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
-
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force `
-                        -ErrorAction Stop
-                } | Should -Not -Throw
-            }
-
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
-
-            It 'Should have set the resource and all the parameters should match' {
-                $current = Get-DscConfiguration | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-
-                $current.Name        | Should -Be $configData.AllNodes[0].Name
-                $current.Namespace   | Should -Be $configData.AllNodes[0].Namespace
-                $current.NameServers | Should -Be $configData.AllNodes[0].NameServers
-                $current.Ensure      | Should -Be $configData.AllNodes[0].Ensure
-            }
-
-            It 'Should have created the NRPT rule' {
-                Get-DnsClientNrptRule -Name $script:dummyRule.Name -ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
-            }
         }
 
-        Describe "$($script:dscResourceName)_Remove_Integration" {
+        AfterEach {
+            Wait-ForIdleLcm
+        }
+
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configData
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+            }
+
+            $current.Name        | Should -Be $configData.AllNodes[0].Name
+            $current.Namespace   | Should -Be $configData.AllNodes[0].Namespace
+            $current.NameServers | Should -Be $configData.AllNodes[0].NameServers
+            $current.Ensure      | Should -Be $configData.AllNodes[0].Ensure
+        }
+
+        It 'Should have created the NRPT rule' {
+            Get-DnsClientNrptRule -Name $script:dummyRule.Name -ErrorAction SilentlyContinue | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Describe "$($script:dscResourceName)_Remove_Integration" {
+        BeforeAll {
             $configData = @{
                 AllNodes = @(
                     @{
@@ -88,54 +135,49 @@ try
                         Name        = $script:dummyRule.Name
                         Namespace   = $script:dummyRule.Namespace
                         NameServers = $script:dummyRule.NameServers
-                        Ensure      = 'Absent'
+                        Ensure      = 'Present'
                     }
                 )
             }
+        }
 
-            It 'Should compile and apply the MOF without throwing' {
-                {
-                    & "$($script:dscResourceName)_Config" `
-                        -OutputPath $TestDrive `
-                        -ConfigurationData $configData
+        AfterEach {
+            Wait-ForIdleLcm
+        }
 
-                    Start-DscConfiguration `
-                        -Path $TestDrive `
-                        -ComputerName localhost `
-                        -Wait `
-                        -Verbose `
-                        -Force `
-                        -ErrorAction Stop
-                } | Should -Not -Throw
+        It 'Should compile and apply the MOF without throwing' {
+            {
+                & "$($script:dscResourceName)_Config" `
+                    -OutputPath $TestDrive `
+                    -ConfigurationData $configData
+
+                Start-DscConfiguration `
+                    -Path $TestDrive `
+                    -ComputerName localhost `
+                    -Wait `
+                    -Verbose `
+                    -Force `
+                    -ErrorAction Stop
+            } | Should -Not -Throw
+        }
+
+        It 'Should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
+        }
+
+        It 'Should have set the resource and all the parameters should match' {
+            $current = Get-DscConfiguration | Where-Object -FilterScript {
+                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
             }
 
-            It 'Should be able to call Get-DscConfiguration without throwing' {
-                { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -Throw
-            }
+            $current.Name        | Should -Be $configData.AllNodes[0].Name
+            $current.Namespace   | Should -Be $configData.AllNodes[0].Namespace
+            $current.NameServers | Should -Be $configData.AllNodes[0].NameServers
+            $current.Ensure      | Should -Be $configData.AllNodes[0].Ensure
+        }
 
-            It 'Should have set the resource and all the parameters should match' {
-                $current = Get-DscConfiguration | Where-Object -FilterScript {
-                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
-                }
-
-                $current.Name        | Should -Be $configData.AllNodes[0].Name
-                $current.Namespace   | Should -Be $configData.AllNodes[0].Namespace
-                $current.NameServers | Should -Be $configData.AllNodes[0].NameServers
-                $current.Ensure      | Should -Be $configData.AllNodes[0].Ensure
-            }
-
-            It 'Should have deleted the NRPT rule' {
-                Get-DnsClientNrptRule -Name $script:dummyRule.Name | Should -BeNullOrEmpty
-            }
+        It 'Should have deleted the NRPT rule' {
+            Get-DnsClientNrptRule -Name $script:dummyRule.Name | Should -BeNullOrEmpty
         }
     }
-}
-finally
-{
-    # Clean up any created rules just in case the integration tests fail
-    $null = Remove-DnsClientNrptRule $script:dummyRule.Name `
-        -Force `
-        -ErrorAction SilentlyContinue
-
-    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
